c551b70616d04e441dc351db02dab002bcc49417
Trent Billington <pudgepacket@gmail.com>
2016-08-16 16:51:31 +1000

    Rename components dir to api-components, uicomponents to ui-components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 // Api components
-import { Aggregate, BucketAggregate, CountAggregate, MetricAggregate } from './components/Aggregate.js';
-import Body from './components/Body.js';
-import Fields from './components/Fields.js';
-import Filter from './components/Filter.js';
-import { FieldIndexBoost, ScoreIndexBoost } from './components/IndexBoost.js';
-import { DistanceMetaBoost, ElementMetaBoost, GeoMetaBoost, IntervalMetaBoost, TextMetaBoost, FilterMetaBoost } from './components/MetaBoost.js';
-import MaxResults from './components/MaxResults.js';
-import Page from './components/Page.js';
-import RegisterNamespace from './components/RegisterNamespace.js';
-import ResultInjector from './components/ResultInjector.js';
-import Sort from './components/Sort.js';
-import Transform from './components/Transform.js';
-import Run from './components/Run.js';
+import { Aggregate, BucketAggregate, CountAggregate, MetricAggregate } from './api-components/Aggregate.js';
+import Body from './api-components/Body.js';
+import Fields from './api-components/Fields.js';
+import Filter from './api-components/Filter.js';
+import { FieldIndexBoost, ScoreIndexBoost } from './api-components/IndexBoost.js';
+import { DistanceMetaBoost, ElementMetaBoost, GeoMetaBoost, IntervalMetaBoost, TextMetaBoost, FilterMetaBoost } from './api-components/MetaBoost.js';
+import MaxResults from './api-components/MaxResults.js';
+import Page from './api-components/Page.js';
+import RegisterNamespace from './api-components/RegisterNamespace.js';
+import ResultInjector from './api-components/ResultInjector.js';
+import Sort from './api-components/Sort.js';
+import Transform from './api-components/Transform.js';
+import Run from './api-components/Run.js';
 
 // Premade, bare-bones html elements wrapping api components
-import BodyInput from './uicomponents/BodyInput.js';
-import FilterSelect from './uicomponents/FilterSelect.js';
-import MaxResultsSelect from './uicomponents/MaxResultsSelect.js';
-import Pagination from './uicomponents/Pagination.js';
-import RequestPreview from './uicomponents/RequestPreview.js';
-import ResultRenderer from './uicomponents/ResultRenderer.js';
-import SortSelect from './uicomponents/SortSelect.js';
+import BodyInput from './ui-components/BodyInput.js';
+import FilterSelect from './ui-components/FilterSelect.js';
+import MaxResultsSelect from './ui-components/MaxResultsSelect.js';
+import Pagination from './ui-components/Pagination.js';
+import RequestPreview from './ui-components/RequestPreview.js';
+import ResultRenderer from './ui-components/ResultRenderer.js';
+import SortSelect from './ui-components/SortSelect.js';
 
 const SearchComponents = {
   Aggregate, BucketAggregate, CountAggregate, MetricAggregate,
