fda25cbb30d8b57cc07511ccc8865df6407ae377
Trent Billington <pudgepacket@gmail.com>
2016-10-06 15:44:39 +1100

    rename ord prop on Sort to order

--- src/api-components/Sort.js
+++ src/api-components/Sort.js
@@ -5,20 +5,20 @@ import Base from './Base.js';
 import Components from '../constants/QueryComponentConstants.js';
 
 const Sort = props => {
-  const { field, ord, ...others } = props;
+  const { field, order, ...others } = props;
   return (
     <Base
       {...others}
       runDefault='update'
       componentName={Components.SORT}
-      data={sort(field, ord)}
+      data={sort(field, order)}
     />
   );
 };
 
 Sort.propTypes = {
   field: React.PropTypes.string.isRequired,
-  ord: React.PropTypes.string.isRequired,
+  order: React.PropTypes.string.isRequired,
 };
 
 export default Sort;
