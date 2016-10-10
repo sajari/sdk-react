dec8d14b1df475d18d3e06b324de6f82fe4abb5a
Trent Billington <pudgepacket@gmail.com>
2016-10-11 10:48:17 +1100

    Update BodyInput to reflect changes in the SDK

--- src/ui-components/web/BodyInput.js
+++ src/ui-components/web/BodyInput.js
@@ -1,46 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-import Body from '../api-components/Body.js';
+import SearchActions from '../../actions/SearchActions'
+import Body from '../../components/Body'
+import PageActions from '../../actions/PageActions'
+import PageStore from '../../stores/PageStore'
 
-class BodyInput extends React.Component {
+class BodyInput extends Component {
   constructor(props) {
-    super(props);
-    this.state = { text: props.body };
-    this.onInputChange = this.onInputChange.bind(this);
+    super(props)
+    this.state = { text: props.text }
+    this.onChange = this.onChange.bind(this)
   }
 
-  onInputChange(evt) {
-    this.setState({ text: evt.target.value });
+  onChange(evt) {
+    const text = evt.target.value
+    if (text.length === 0 || !this.state.text.startsWith(text.slice(0, 3))) {
+      SearchActions.resetTracking()
+    }
+    if (text !== this.state.text && PageStore.get(this.props.namespace) !== 1) {
+      PageActions.set(this.props.namespace, 1)
+    }
+    this.setState({ text })
   }
 
   render() {
-    const { namespace, ...others } = this.props;
-
+    // Take out text to prevent it going into others
+    const { text: _, minLength, prefixBoosts, containsBoosts, namespace, ...others } = this.props
     return (
       <div>
-        <input
-          type='text'
-          value={this.state.text}
-          onChange={this.onInputChange}
-        />
+        <input type="text" onChange={this.onChange} {...others} />
         <Body
-          {...others}
+          text={this.state.text}
+          minLength={minLength}
+          prefixBoosts={prefixBoosts}
+          containsBoosts={containsBoosts}
           namespace={namespace}
-          body={this.state.text}
         />
       </div>
-    );
+    )
   }
 }
 
 BodyInput.propTypes = {
-  body: React.PropTypes.string,
+  text: React.PropTypes.string.isRequired,
+  minLength: React.PropTypes.number,
+  prefixBoosts: React.PropTypes.object,
+  containsBoosts: React.PropTypes.object,
   namespace: React.PropTypes.string,
-};
+}
 
 BodyInput.defaultProps = {
-  body: '',
+  text: '',
+  minLength: 3,
+  prefixBoosts: {},
+  containsBoosts: {},
   namespace: 'default',
-};
+}
 
 export default BodyInput;
