6aece9d9c6aab89511cac00924eee2180a0d528c
Trent Billington <pudgepacket@gmail.com>
2016-10-11 10:46:20 +1100

    Remove unnecessary Page interactions from Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,71 +3,19 @@ import React, { Component } from 'react'
 import { fieldFilter, FILTER_OP_HAS_PREFIX, FILTER_OP_CONTAINS } from 'sajari'
 
 import { default as BaseBody } from '../api-components/Body'
-import Page from '../api-components/Page'
 import { FilterFieldBoost } from '../api-components'
 import { ALL } from '../constants/RunModes'
-
-class InputBody extends Component {
-  constructor() {
-    this.state = {
-      text: this.props.text
-    }
-    this.onChange = this.onChange.bind(this)
-  }
-
-  onChange(evt) {
-    const v = evt.target.value
-    if (v.length === 0 || !this.state.text.startswith(v.slice(0, 3))) {
-      SearchActions.resetTracking()
-    }
-    this.setState({
-      text: v
-    })
-  }
-
-  render() {
-    // Take out text to prevent it going into others
-    const { text as _, minLength, prefixBoosts, containsBoosts, namespace, ...others } = this.props
-    return (
-      <div>
-        <input type="text" onChange={this.onChange} {...others} />
-        <Body
-          text={this.state.text}
-          minLength={minLength}
-          prefixBoosts={prefixBoosts}
-          containsBoosts={containsBoosts}
-          namespace={namespace}
-        />
-      </div>
-    )
-
-  }
-}
 
 class Body extends Component {
   constructor(props) {
     super(props)
-
+    this.state = {}
     this.onPageChange = this.onPageChange.bind(this)
   }
 
   shouldComponentUpdate(newProps) {
     // Don't update the component if the text doesn't satisfy the minimum length
     return newProps.text.length >= newProps.minLength
-  }
-
-  componentDidMount() {
-    PageStore.AddChangeListener(this.onPageChange)
-  }
-
-  componentWillUnmount() {
-    PageStore.RemoveChangeListener(this.onPageChange)
-  }
-
-  onPageChange() {
-    this.setState({
-      page: PageStore.get()
-    })
   }
 
   render() {
@@ -106,8 +54,7 @@ class Body extends Component {
       <div>
         {prefixBoostComponents}
         {containsBoostComponents}
-        <Page page={this.state.page} namespace={namespace} />
-        <BaseBody body={text} run={ALL} weight={1} namespace={namespace} trackingResetTrigger={trackingResetFns} />
+        <BaseBody body={text} run={ALL} weight={1} namespace={namespace} />
       </div>
     )
   }
