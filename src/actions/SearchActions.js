47e554f4e9a0ef11a34ab6b952fc71c7090bd608
Trent Billington <pudgepacket@gmail.com>
2016-11-10 15:22:59 +1100

    Fix missing comma

--- src/actions/SearchActions.js
+++ src/actions/SearchActions.js
@@ -150,7 +150,7 @@ const SearchActions = {
       actionType: SearchConstants.TRACKING_RESET,
       namespace,
     })
-  }
+  },
 
   clearResults(namespace) {
     AppDispatcher.handleViewAction({
