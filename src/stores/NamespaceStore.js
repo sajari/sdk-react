8b915ed14521891c827cca7c7d4f592f9dcbaa89
Trent Billington <pudgepacket@gmail.com>
2016-11-18 12:07:50 +1100

    Remove immutable from NamespaceStore

--- src/stores/NamespaceStore.js
+++ src/stores/NamespaceStore.js
@@ -1,39 +1,17 @@
-import { Map as map } from 'immutable';
-
 import { ChangeEmitter } from '../utils/ChangeEmitter.js';
 import AppDispatcher from '../dispatcher/AppDispatcher.js';
 import NamespaceConstants from '../constants/NamespaceConstants.js';
 
-let data = map();
-
-function k(project, collection, engine) {
-  return { project, collection, engine };
-}
-
-function get(namespace) {
-  return data.get(namespace);
-}
-
-function getAll() {
-  return data;
-}
+let data = {}
 
 class NamespaceStore extends ChangeEmitter {
-  get(namespace) {
-    return get(namespace);
+  get(namespace = 'default') {
+    return data[namespace]
   }
 
   getAll() {
-    return getAll();
+    return data
   }
-}
-
-function set(project, collection, namespace, engine) {
-  data = data.set(namespace, k(project, collection, engine));
-}
-
-function remove(namespace) {
-  data = data.delete(namespace);
 }
 
 const namespaceStore = new NamespaceStore();
@@ -44,10 +22,17 @@ namespaceStore.dispatchToken = AppDispatcher.register(payload => {
 
   if (source === 'NAMESPACE_ACTION') {
     if (action.actionType === NamespaceConstants.SET) {
-      set(action.project, action.collection, action.namespace, action.engine);
+      data = {
+        ...data,
+        [action.namespace]: {
+          project: action.project,
+          collection: action.collection,
+        }
+      }
       namespaceStore.emitChange();
     } else if (action.actionType === NamespaceConstants.REMOVE) {
-      remove(action.namespace);
+      let { [action.namespace]: _, ...rest } = data
+      data = rest
       namespaceStore.emitChange();
     }
   }
