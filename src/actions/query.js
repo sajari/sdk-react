4f139db64dcb4a4083bcfe7be6ca3176a241e1a6
David Howden <dhowden@gmail.com>
2016-11-23 12:05:06 +1100

    Small code refactoring and added TODOs.

--- src/actions/query.js
+++ src/actions/query.js
@@ -1,19 +1,9 @@
 import * as Sajari from 'sajari'
 import SearchComponents from '../constants/QueryComponentConstants'
 
+
 export const NAMESPACE_ADD = 'NAMESPACE_ADD'
 export const NAMESPACE_REMOVE = 'NAMESPACE_REMOVE'
-
-export const QUERY_COMPONENT_ADD = 'QUERY_COMPONENT_ADD'
-export const QUERY_COMPONENT_MODIFY = 'QUERY_COMPONENT_MODIFY'
-export const QUERY_COMPONENT_REMOVE = 'QUERY_COMPONENT_REMOVE'
-
-export const SEARCH_REQUEST = 'SEARCH_REQUEST'
-export const SEARCH_REQUEST_SUCCESS = 'SEARCH_REQUEST_SUCCESS'
-export const SEARCH_REQUEST_FAILURE = 'SEARCH_REQUEST_FAILURE'
-
-export const QUERY_TRACKING_SET = 'QUERY_TRACKING_SET'
-export const QUERY_TRACKING_FLUSH = 'QUERY_TRACKING_FLUSH'
 
 export const addNamespace = (namespace, project, collection) => ({
   type: NAMESPACE_ADD,
@@ -21,10 +11,16 @@ export const addNamespace = (namespace, project, collection) => ({
   project,
   collection,
 })
+
 export const removeNamespace = (namespace) => ({
   type: NAMESPACE_REMOVE,
   namespace,
 })
+
+
+export const QUERY_COMPONENT_ADD = 'QUERY_COMPONENT_ADD'
+export const QUERY_COMPONENT_MODIFY = 'QUERY_COMPONENT_MODIFY'
+export const QUERY_COMPONENT_REMOVE = 'QUERY_COMPONENT_REMOVE'
 
 export const addQueryComponent = (uuid, namespace, data, queryDataType) => ({
   type: QUERY_COMPONENT_ADD,
@@ -33,6 +29,7 @@ export const addQueryComponent = (uuid, namespace, data, queryDataType) => ({
   data,
   queryDataType,
 })
+
 export const modifyQueryComponent = (uuid, namespace, data, queryDataType) => ({
   type: QUERY_COMPONENT_MODIFY,
   uuid,
@@ -40,11 +37,17 @@ export const modifyQueryComponent = (uuid, namespace, data, queryDataType) => ({
   data,
   queryDataType,
 })
+
 export const removeQueryComponent = (uuid, namespace) => ({
   type: QUERY_COMPONENT_MODIFY,
   uuid,
   namespace,
 })
+
+
+export const SEARCH_REQUEST = 'SEARCH_REQUEST'
+export const SEARCH_REQUEST_SUCCESS = 'SEARCH_REQUEST_SUCCESS'
+export const SEARCH_REQUEST_FAILURE = 'SEARCH_REQUEST_FAILURE'
 
 export const searchRequest = (namespace) => ({
   type: SEARCH_REQUEST,
@@ -60,55 +63,71 @@ export const searchRequestFailure = (namespace, error) => ({
   namespace,
   error,
 })
+
+
+export const QUERY_TRACKING_SET = 'QUERY_TRACKING_SET'
+export const QUERY_TRACKING_FLUSH = 'QUERY_TRACKING_FLUSH'
+
+export const setQueryTracking = (namespace, data, id, sequence) => ({
+  type: QUERY_TRACKING_SET,
+  namespace,
+  data,
+  id,
+  sequence,
+})
+export const flushQueryTracking = (namespace) => ({
+  type: QUERY_TRACKING_FLUSH,
+  namespace,
+})
+
+
 export const makeSearchRequest = (namespace) => (
   (dispatch, getState) => {
     dispatch(searchRequest(namespace))
 
     const state = getState()
 
-    const { project, collection } = state.query.namespaces[namespace]
-    const client = new Sajari.Client(project, collection)
     const query = new Sajari.Query()
 
-    const trackingFromState = state.query.queryTracking[namespace]
-    if (trackingFromState) {
-      query.data = trackingFromState.data
-      query.i = trackingFromState.id,
-      query.s = trackingFromState.sequence
-    }
-
     const components = state.query.queryComponent[namespace]
+
+    // TODO(tbillington): pull out all the data and build a dict so that you don't have
+    // to keep running through all the components for each query property you set.
 
     const indexQuery = new Sajari.IndexQuery()
     indexQuery.body(getDataOfType(components, SearchComponents.BODY))
     indexQuery.instanceBoosts(getDataOfType(components, SearchComponents.INSTANCE_BOOSTS))
     indexQuery.fieldBoosts(getDataOfType(components, SearchComponents.FIELD_BOOSTS))
-
+    query.indexQuery(indexQuery)
+    
     const featureQuery = new Sajari.FeatureQuery()
     featureQuery.fieldBoosts(getDataOfType(components, SearchComponents.FEATURE_BOOST))
+    query.featureQuery(featureQuery)
 
     query.fields(getDataOfType(components, SearchComponents.FIELDS).reduce((a, b) => a.concat(b)))
     const limit = getDataOfType(components, SearchComponents.LIMIT)
+    // TODO(tbillington): throw an error if more than one is set.
     if (limit.length > 0) {
       query.limit(limit[0])
     }
+    // TODO(tbillington): throw an error if more than one is set.
     const offset = getDataOfType(components, SearchComponents.OFFSET)
     if (offset.length > 0) {
       query.offset(offset[0])
     }
 
-    query.indexQuery(indexQuery)
-    query.featureQuery(featureQuery)
     query.transforms(getDataOfType(components, SearchComponents.TRANSFORM))
     query.sort(getDataOfType(components, SearchComponents.SORT))
 
     const filters = getDataOfType(components, SearchComponents.FILTER)
+    // TODO(tbillington): tempted to say through an error here too... (when more than one is specified).
     if (filters.length === 1) {
       query.filter(filters[0])
     } else if (filters.length > 1) {
       query.filter(Sajari.allFilters(filters))
     }
 
+    // TODO(tbillington): throw an error here if more than one is set.
     const clickTokens = getDataOfType(components, SearchComponents.CLICK_TOKENS)
     const posNegTokens = getDataOfType(components, SearchComponents.POS_NEG_TOKENS)
     if (clickTokens.length > 0) {
@@ -118,6 +137,16 @@ export const makeSearchRequest = (namespace) => (
     }
 
     query.aggregates(getDataOfType(components, SearchComponents.AGGREGATE))
+
+    const trackingFromState = state.query.queryTracking[namespace]
+    if (trackingFromState) {
+      query.data = trackingFromState.data
+      query.i = trackingFromState.id,
+      query.s = trackingFromState.sequence
+    }
+
+    const { project, collection } = state.query.namespaces[namespace]
+    const client = new Sajari.Client(project, collection)
 
     const searchPromise = client.search(query, (err, res) => {
       if (err) {
@@ -136,15 +165,3 @@ export const makeSearchRequest = (namespace) => (
 function getDataOfType(components, type) {
   return Object.keys(components).map(k => components[k]).filter(d => d.type === type).map(d => d.data)
 }
-
-export const setQueryTracking = (namespace, data, id, sequence) => ({
-  type: QUERY_TRACKING_SET,
-  namespace,
-  data,
-  id,
-  sequence,
-})
-export const flushQueryTracking = (namespace) => ({
-  type: QUERY_TRACKING_FLUSH,
-  namespace,
-})
