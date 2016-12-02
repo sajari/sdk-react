080ad550e0a8050681e97bd2431c427c464dbc3b
Trent Billington <pudgepacket@gmail.com>
2016-12-02 17:04:11 +1100

    Add lint to build step

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -14,7 +14,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: ["react-hot-loader", "babel-loader?stage=0", "eslint-loader"],
-      },
+      }
     ],
   },
-};
+  eslint: {
+    configFile: '.eslintrc',
+    emitError: true
+  }
