c358811607a4f7b4f8b4817c03551882e09e4708
Trent Billington <pudgepacket@gmail.com>
2016-12-08 16:03:20 +1100

    ui: Add events and basic ga analytics system

---
+++ src/ui/Analytics/ga.js
@@ -0,0 +1,92 @@
+const url = {
+
+  /**
+   * Convert a query string in to an object
+   */
+  decodeUriArgs : function(queryStr) {
+    const args = {}
+    const a = queryStr.split('&')
+    for (const i in a) {
+      if (a.hasOwnProperty(i)) {
+        const b = a[i].split('=')
+        args[decodeURIComponent(b[0])] = decodeURIComponent(b[1])
+      }
+    }
+    return args
+  },
+
+  /**
+   * Convert an arguments object in to a query string
+   */
+  encodeUriArgs : function(args) {
+    const queryParts = []
+    for (const i in args) {
+      queryParts.push(encodeURIComponent(i)+'='+encodeURIComponent(args[i]))
+    }
+    return queryParts.join('&')
+  },
+
+  /**
+   * Merges query strings or objects into a single query string. Accepts a variable number of query string/objects
+   * to merge. The latter overrides the former.
+   */
+  mergeQueryStr : function(queryStr1) {
+    const args = (typeof queryStr1 === 'string' ? this.decodeUriArgs(queryStr1) : queryStr1)
+    for (let i = 1; i < arguments.length; i++) {
+      if (arguments[i] !== undefined) {
+        const nextArgs = (typeof arguments[i] === 'string' ? this.decodeUriArgs(arguments[i]) : arguments[i])
+        for (const a in nextArgs) {
+          args[a] = nextArgs[a]
+        }
+      }
+    }
+    return this.encodeUriArgs(args)
+  },
+
+  /**
+   * Takes an existing URL and merges additional data into the query string
+   */
+  augmentUri : function(uri, args) {
+    const m = /^([^\?]+)\?(.+)+$/.exec(uri)
+    if (m) {
+      return m[1]+'?'+this.mergeQueryStr(m[2], args)
+    } else {
+      return uri+'?'+this.encodeUriArgs(args)
+    }
+  },
+
+  /**
+   * Get a parameter from the URL
+   */
+  getURLParameter : function(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,''])[1].replace(/\+/g, '%20'))||null
+  }
+}
+
+function sendGAPageView(body) {
+  if (window.ga && process.env.NODE_ENV.environment !== 'development') {
+    // Merge the body in with the existing query params in the url
+    const pageAddress = url.augmentUri(
+      // Take only the portion of the url following the domain
+      location.href.substring(location.origin.length),
+      { q: body }
+    )
+    window.ga('send', 'pageview', pageAddress)
+  }
+}
+
+class GoogleAnalytics {
+  onBodyReset(previousBody) {
+    sendGAPageView(previousBody)
+  }
+
+  onResultClicked(body) {
+    sendGAPageView(body)
+  }
+
+  onPageClose(body) {
+    sendGAPageView(body)
+  }
+}
+
+export default GoogleAnalytics
---
+++ src/ui/middleware/Events.js
@@ -0,0 +1,56 @@
+import { SET_BODY } from '../actions/Search'
+import { RESULT_CLICKED } from '../actions/Analytics'
+import { SEARCH_REQUEST_SUCCESS } from '../../api/actions/query'
+
+let data = {
+  analyticsEnabled: false,
+  body: ''
+}
+
+const events = (analytics) => {
+  window.addEventListener('beforeunload', () => {
+    if (data.analyticsEnabled && data.body) {
+      analytics.onPageClose(data.body)
+    }
+  })
+
+  return ({ getState }) => (next) => (action) => {
+    if (action.type === SEARCH_REQUEST_SUCCESS) {
+      data = {
+        ...data,
+        analyticsEnabled: true,
+        body: getState().search.body,
+      }
+    }
+
+    if (action.type === SET_BODY) {
+
+      const previousBody = getState().search.body || ''
+      const newBody = action.body
+
+      if (previousBody.length >= 2 && newBody.length < 2) {
+        if (data.analyticsEnabled && data.body) {
+          analytics.onBodyReset(previousBody)
+        }
+        data = { ...data, body: '' }
+      }
+    }
+
+    if (action.type === RESULT_CLICKED) {
+      if (data.analyticsEnabled && data.body) {
+        analytics.onResultClicked(getState().search.body)
+      }
+      data = { ...data, body: '' }
+    }
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action)
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue
+  }
+}
+
+
+export default events
