9ad2d50823bfb7d212e3f33f3a68b091351e191f
Trent Billington <pudgepacket@gmail.com>
2016-12-08 17:11:48 +1100

    overlay example: update to latest sajari react

--- examples/overlay/src/index.js
+++ examples/overlay/src/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'sajari-react/ui/Overlay.css'
-import 'sajari-react/ui/Search.css'
-// import './index.css'
-
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
-import { DefaultOverlay } from 'sajari-react/ui/Overlay'
-import { setActive } from 'sajari-react/ui/actions/Overlay'
+
 import Config from 'sajari-react/ui/Config'
 
-import makeDefaultStore from './store'
+import query from 'sajari-react/api/reducers/query'
+import overlay from 'sajari-react/ui/reducers/Overlay'
+import search from 'sajari-react/ui/reducers/Search'
+import tabs from 'sajari-react/ui/reducers/Tabs'
+import { setActive } from 'sajari-react/ui/actions/Overlay'
+import { DefaultOverlay } from 'sajari-react/ui/Overlay'
+import { resetPageMiddleware, searchMiddleware } from 'sajari-react/ui/middleware/Search'
+import { resetOverlayMiddleware } from 'sajari-react/ui/middleware/Overlay'
 
+import events from 'sajari-react/ui/middleware/Events'
+import GoogleAnalytics from 'sajari-react/ui/Analytics/ga'
 
-const store = makeDefaultStore()
+// import App from './App'
+
+import 'sajari-react/ui/Search.css'
+import 'sajari-react/ui/Overlay.css'
+
+import './Override.css'
+
+const store = createStore(
+  combineReducers({ overlay, query, search, tabs }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunkMiddleware, resetOverlayMiddleware(), resetPageMiddleware, searchMiddleware, events(new GoogleAnalytics()))
+)
 
 store.dispatch(setActive(true))
 
