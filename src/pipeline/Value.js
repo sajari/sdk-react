3e352650d4a517b5bc0fc67bb0eb34bb1fe21b06
Trent Billington <pudgepacket@gmail.com>
2017-02-27 17:32:05 +1100

    Fix pipeline Value component

--- src/pipeline/Value.js
+++ src/pipeline/Value.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import uuid from 'uuid'
 import { connect } from 'react-redux'
 
-import Components from './constants/QueryComponentConstants.js'
 import {
   addPipelineValue, modifyPipelineValue, removePipelineValue,
-  changePipelineValueNamespace, makeSearchRequest
-} from './actions/pipeline'
+  changePipelineValueNamespace, makePipelineSearchRequest
+} from '../api/actions/pipeline'
 
 class value extends React.Component {
   constructor(props) {
@@ -18,7 +16,7 @@ class value extends React.Component {
     this.props.addPipelineValue(namespace, pipeline, name, value)
 
     if (this.props.runOnMount) {
-      this.props.makeSearchRequest()
+      this.props.makePipelineSearchRequest()
     }
   }
 
@@ -29,7 +27,7 @@ class value extends React.Component {
       // The namespace has not changed so we can modify the query component value in place
       this.props.modifyPipelineValue(newNamespace, pipeline, name, value)
       if (newProps.runOnUpdate) {
-        this.props.makeSearchRequest()
+        this.props.makePipelineSearchRequest()
       }
     } else {
       // The namespace has changed so we must remove the component from the old namespace and add it to the new
@@ -42,30 +40,11 @@ class value extends React.Component {
     this.props.removePipelineValue(namespace, pipeline, name)
 
     if (this.props.runOnUnmount) {
-      this.props.makeSearchRequest()
+      this.props.makePipelineSearchRequest()
     }
   }
 
   render() { return null }
-}
-
-base.propTypes = {
-  runOnMount: React.PropTypes.bool,
-  runOnUnmount: React.PropTypes.bool,
-  runOnUpdate: React.PropTypes.bool,
-  namespace: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.arrayOf(React.PropTypes.string),
-  ]),
-  name: React.PropTypes.string.isRequired,
-  value: React.PropTypes.string.isRequired,
-}
-
-base.defaultProps = {
-  runOnMount: false,
-  runOnUnmount: false,
-  runOnUpdate: false,
-  namespace: 'default',
 }
 
 const Value = connect(
@@ -75,8 +54,28 @@ const Value = connect(
     modifyPipelineValue: (namespace, pipeline, name, value) => dispatch(modifyPipelineValue(namespace, pipeline, name, value)),
     changePipelineValueNamespace: (oldNamespace, newNamespace) => dispatch(changePipelineValueNamespace(oldNamespace, newNamespace)),
     removePipelineValue: (namespace, pipeline, name) => dispatch(removePipelineValue(namespace, pipeline, name)),
-    makeSearchRequest: () => dispatch(makeSearchRequest(props.namespace)),
+    makePipelineSearchRequest: () => dispatch(makePipelineSearchRequest(props.namespace, props.pipeline)),
   }),
-)(base)
+)(value)
+
+Value.propTypes = {
+  runOnMount: React.PropTypes.bool,
+  runOnUnmount: React.PropTypes.bool,
+  runOnUpdate: React.PropTypes.bool,
+  namespace: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.arrayOf(React.PropTypes.string),
+  ]),
+  pipeline: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  value: React.PropTypes.string.isRequired,
+}
+
+Value.defaultProps = {
+  runOnMount: false,
+  runOnUnmount: false,
+  runOnUpdate: false,
+  namespace: 'default',
+}
 
 export default Value
