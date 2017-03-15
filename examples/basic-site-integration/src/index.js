e9c0dc1c6a7075e727d99f40052f29189ba4d172
Trent Billington <pudgepacket@gmail.com>
2017-03-15 16:48:27 +1100

    Add search middleware and GA to site search integration example

--- examples/basic-site-integration/src/index.js
+++ examples/basic-site-integration/src/index.js
@@ -5,6 +5,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import pipelines from 'sajari-react/api/reducers/pipeline'
+import search from 'sajari-react/ui/reducers/Search'
+import { resetPipelinePageMiddleware, searchPipelineMiddleware } from 'sajari-react/ui/middleware/Search'
+import GoogleAnalytics from 'sajari-react/ui/Analytics/ga'
+import AnalyticsEvents from 'sajari-react/ui/middleware/Events'
 
 import App from './App'
 
@@ -15,9 +19,9 @@ function sjsi(config) {
   }
 
   const store = createStore(
-    combineReducers({ pipelines }),
+    combineReducers({ pipelines, search }),
     process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunkMiddleware)
+    applyMiddleware(thunkMiddleware, resetPipelinePageMiddleware, searchPipelineMiddleware, AnalyticsEvents(new GoogleAnalytics()))
   )
 
   ReactDOM.render(
