87132039b4fb37a011a2693248269a84d83788ac
Trent Billington <pudgepacket@gmail.com>
2017-04-27 14:24:56 +1000

    added images to search interface

--- src/ui/Results.js
+++ src/ui/Results.js
@@ -81,20 +81,18 @@ Image.propTypes = {
 
 const Result = ({ title, description, url, token, showImage, image, resultClicked }) => (
   <div className='sj-result'>
+    {showImage && image ? (
+      <div className="sj-result-image-container">
+        <TokenLink token={token} url={url}>
+          <Image url={image} title={title} />
+        </TokenLink>
+      </div>
+    ) : null}
     <div className="sj-result-text">
       <Title title={title} url={url} token={token} resultClicked={resultClicked} />
       <Description description={description} />
       <Url url={url} token={token} resultClicked={resultClicked} />
     </div>
-    {showImage ? (
-      <div className="sj-result-image">
-        <div className="sj-result-image-container">
-          <TokenLink token={token} url={url}>
-            <Image url={image} title={title} />
-          </TokenLink>
-        </div>
-      </div>
-    ) : null}
   </div>
 )
 
