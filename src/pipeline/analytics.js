9442f84e7eb9c2613bb2f1762531dee5af8beaee
Trent Billington <pudgepacket@gmail.com>
2017-04-28 10:46:39 +1000

    analytics: handle reset body case properly

--- src/pipeline/analytics.js
+++ src/pipeline/analytics.js
@@ -1,6 +1,11 @@
 import GA from "sajari-react/ui/Analytics/ga";
 
-import { State, RESULTS_CHANGED, TRACKING_RESET, RESULT_CLICKED } from './state'
+import {
+  State,
+  RESULTS_CHANGED,
+  TRACKING_RESET,
+  RESULT_CLICKED
+} from "./state";
 
 class Analytics {
   constructor(namespace, ga = new GA()) {
@@ -10,6 +15,14 @@ class Analytics {
 
     this.enabled = false;
     this.body = "";
+
+    // longest values are for sending the users last intended query on reset
+    this.longestNonAutocompletedBody = "";
+    this.longestAutocompletedBody = "";
+
+    // default to working with web pipeline values
+    this.bodyLabel = "q";
+    this.bodyAutocompletedLabel = "q.used";
 
     this.beforeunload = this.beforeunload.bind(this);
     this.onChange = this.onChange.bind(this);
@@ -32,7 +45,13 @@ class Analytics {
 
   resetBody() {
     if (this.enabled) {
-      this.ga.onBodyReset(this.body);
+      // Send the longest body since the last time the body was cleared.
+      // Use completion if available.
+      this.ga.onBodyReset(
+        this.longestAutocompletedBody || this.longestNonAutocompletedBody
+      );
+      this.longestNonAutocompletedBody = "";
+      this.longestAutocompletedBody = "";
       this.enabled = false;
     }
   }
@@ -45,7 +64,18 @@ class Analytics {
     }
 
     const values = State.ns(this.namespace).getValues();
-    const newBody = values["q.used"] || values.q || "";
+    const originalBody = values[this.bodyLabel] || "";
+    const newBody = values[this.bodyAutocompletedLabel] || originalBody;
+
+    // Here we check the lengths of the non-autocompleted bodies.
+    // We do this because while the user is backspacing their query
+    // the new autocompleted body may be longer than their actual input,
+    // but we want to record their input rather than a completion resulting
+    // from them removing chars.
+    if (originalBody.length >= this.longestNonAutocompletedBody.length) {
+      this.longestNonAutocompletedBody = originalBody;
+      this.longestAutocompletedBody = values[this.bodyAutocompletedLabel];
+    }
 
     this.body = newBody;
   }
@@ -53,9 +83,11 @@ class Analytics {
   resultClicked() {
     if (this.enabled && this.body) {
       this.ga.onResultClicked(this.body);
+      this.longestNonAutocompletedBody = "";
+      this.longestAutocompletedBody = "";
       this.enabled = false;
     }
   }
 }
 
-export default Analytics
+export default Analytics;
