f862078d0ddfbf9a4b7656941066afc677439192
Trent Billington <pudgepacket@gmail.com>
2017-05-19 16:43:48 +1000

    site-search-overlay: remove redux integration to be in line with sdk

--- examples/basic-site-integration-overlay/src/index.js
+++ examples/basic-site-integration-overlay/src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { createStore, combineReducers } from "redux";
-import { Provider } from "react-redux";
-
-import overlay from "sajari-react/ui/reducers/Overlay";
-import { setActive } from "sajari-react/ui/actions/Overlay";
-
 import loaded from "./loaded";
 
 import App from "./App";
@@ -19,38 +13,24 @@ function sjsi(config) {
     return;
   }
 
-  // Store to hold overlay state
-  const store = createStore(combineReducers({ overlay }));
+  let renderTarget = null;
+  if (config.overlay) {
+    // Create a container to render the overlay into
+    const overlayContainer = document.createElement("div");
+    overlayContainer.id = "sj-overlay-holder";
+    document.body.appendChild(overlayContainer);
 
-  const overlaySetActive = (active = true) => {
-    store.dispatch(setActive(active));
-
-    // This removes the scrollbar on most platforms.
-    // For iOS the implementation is based upon how your content is rendered.
-    document.getElementsByTagName("body")[0].style.overflow = active
-      ? "hidden"
-      : "";
-  };
-
-  // Set a global function which client code can call to launch the overlay
-  window._sjOverlaySetActive = overlaySetActive;
-
-  // If there is a query param supplied, launch the interface
-  if (config.values.q) {
-    overlaySetActive(true);
+    renderTarget = overlayContainer;
+  } else if (config.attachSearchBox) {
+    renderTarget = config.attachSearchBox;
+  } else {
+    error(
+      "no render mode found, need to specify either overlay or attachSearchBox in config"
+    );
+    return;
   }
 
-  // Create a container to render the overlay into
-  const overlayContainer = document.createElement("div");
-  overlayContainer.id = "sj-overlay-holder";
-  document.body.appendChild(overlayContainer);
-
-  ReactDOM.render(
-    <Provider store={store}>
-      <App config={config} closeOverlay={() => overlaySetActive(false)} />
-    </Provider>,
-    overlayContainer
-  );
+  ReactDOM.render(<App config={config} />, renderTarget);
 }
 
 loaded(window, () => sjsi(window._sjsi));
