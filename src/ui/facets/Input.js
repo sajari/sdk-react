505f3c144bf306eb8d47b54ad089893bfed0c16f
Trent Billington <pudgepacket@gmail.com>
2017-07-21 18:15:26 +1000

    facets: add selectfacet

--- src/ui/facets/Input.js
+++ src/ui/facets/Input.js
@@ -12,6 +12,46 @@ class RadioFacet extends React.Component {
   }
 }
 
+class SelectFacet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: props.fb.isSet(props.name) };
+    this.onUpdate = this.onUpdate.bind(this);
+  }
+
+  componentDidMount() {
+    this.unregister = this.props.fb.register(this.onUpdate);
+  }
+
+  componentWillUnmount() {
+    this.unregister();
+  }
+
+  onUpdate(fb) {
+    this.setState({ active: fb.get() });
+  }
+
+  render() {
+    const { fb, name, options, ...other } = this.props;
+    const onClick = e => {
+      fb.set(e.target.value, true);
+    };
+    const optionsRendered = Object.keys(options).map(o => {
+      return (
+        <option value={options[o]}>
+          {o}
+        </option>
+      );
+    });
+    return (
+      <select value={this.state.active} onChange={onClick}>
+        {optionsRendered}
+      </select>
+    );
+    // return <input onClick={onClick} checked={this.state.active} {...other} />;
+  }
+}
+
 class InputFacet extends React.Component {
   constructor(props) {
     super(props);
@@ -32,7 +72,7 @@ class InputFacet extends React.Component {
   }
 
   render() {
-    const { fb, name, ...other } = this.props;
+    const { fb, name, children, ...other } = this.props;
     const onClick = () => {
       fb.set(name, !this.state.active);
     };
@@ -40,4 +80,4 @@ class InputFacet extends React.Component {
   }
 }
 
-export { CheckboxFacet, RadioFacet };
+export { CheckboxFacet, RadioFacet, SelectFacet };
