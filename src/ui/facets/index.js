2f881512d223462c1666ba613e7fd3775de6f875
Trent Billington <pudgepacket@gmail.com>
2017-07-21 12:33:26 +1000

    facets: remove unused facet components

--- src/ui/facets/index.js
+++ src/ui/facets/index.js
@@ -1,4 +1,1 @@
-export { default as CheckFacet } from "./CheckFacet";
-export { default as RadioFacet } from "./RadioFacet";
-export { default as SelectFacet } from "./SelectFacet";
 export { default as TabsFacet } from "./TabsFacet";
