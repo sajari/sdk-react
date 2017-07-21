3f16c509098f1b482dff990a1d8fb849fa9d02c6
Trent Billington <pudgepacket@gmail.com>
2017-07-21 12:36:49 +1000

    ui: move autocomplete input to ui

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,4 +1,5 @@
 import * as facets from "./facets";
 import * as overlay from "./overlay";
 import * as results from "./results";
-export { facets, overlay, results };
+import * as searchInput from "./searchInput";
+export { facets, overlay, results, searchInput };
