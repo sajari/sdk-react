e079169d8f19a6eb2b06e22a2c1f57e1877fcf6e
Trent Billington <pudgepacket@gmail.com>
2017-07-24 10:14:09 +1000

    sdk: fix broken export in ui

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,5 @@
 import * as facets from "./facets";
 import * as overlay from "./overlay";
 import * as results from "./results";
-import * as searchInput from "./searchInput";
-export { facets, overlay, results, searchInput };
+import * as text from "./text";
+export { facets, overlay, results, text };
