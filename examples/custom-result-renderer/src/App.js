131b9cd7166d74c1f693ee22bdf2cdabf3691b20
David Howden <dhowden@gmail.com>
2017-07-26 20:24:53 +1000

    examples/custom-result-rendering: improve example and add css

--- examples/custom-result-renderer/src/App.js
+++ examples/custom-result-renderer/src/App.js
@@ -2,10 +2,20 @@ import React from "react";
 
 import { Pipeline, Values, changeEvent } from "sajari-react/controllers";
 import { AutocompleteInput } from "sajari-react/ui/text";
-import { Response, Results, Summary, Paginator } from "sajari-react/ui/results";
+import {
+  Response,
+  Results,
+  Summary,
+  Paginator,
+  Result,
+  Title,
+  Description,
+  URL
+} from "sajari-react/ui/results";
 
 import "sajari-react/ui/text/AutocompleteInput.css";
 import "sajari-react/ui/results/Paginator.css";
+import "sajari-react/ui/results/Results.css";
 
 import { Client, Tracking } from "sajari";
 
@@ -26,15 +36,40 @@ values.listen(changeEvent, (changes, set) => {
   }
 });
 
-const CustomResult = ({ values, token, onResultClicked }) =>
-  <div>
-    <h1>
-      {values.title}
-    </h1>
-    <p>
-      {values.description}
-    </p>
+// Set the "fields" value to fetch the dir1 field in results.
+values.set({ fields: "title,description,url,dir1" });
+
+// BlogPostResult will be rendered for blog posts.
+const BlogPostResult = ({ values, token, onResultClicked }) =>
+  <div className="sj-result blog-post">
+    <div className="sj-result-text">
+      <div className="label">Blog Post</div>
+      <Title
+        title={values.title}
+        url={values.url}
+        token={token}
+        resultClicked={onResultClicked}
+      />
+      <Description description={values.description} />
+      <URL url={values.url} token={token} resultClicked={onResultClicked} />
+    </div>
   </div>;
+
+// CustomResult chooses between BlogPostResult and the default Result renderer.
+const CustomResult = ({ values, token, onResultClicked }) => {
+  if (values.dir1 === "blog") {
+    return (
+      <BlogPostResult
+        values={values}
+        token={token}
+        onResultClicked={onResultClicked}
+      />
+    );
+  }
+  return (
+    <Result values={values} token={token} onResultClicked={onResultClicked} />
+  );
+};
 
 const App = () =>
   <div className="searchApp">
