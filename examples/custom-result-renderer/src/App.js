24f8003be0f981b0e485b3a713fdb71ce6acd57b
David Howden <dhowden@gmail.com>
2017-07-26 17:17:12 +1000

    examples: added custom-result-renderer

---
+++ examples/custom-result-renderer/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { Pipeline, Values, changeEvent } from "sajari-react/controllers";
+import { AutocompleteInput } from "sajari-react/ui/text";
+import { Response, Results, Summary, Paginator } from "sajari-react/ui/results";
+import "sajari-react/ui/Search.css";
+
+import { Client, Tracking } from "sajari";
+
+const project = "sajariptyltd";
+const collection = "sajari-com";
+const pipelineName = "website";
+
+const values = new Values();
+const client = new Client(project, collection);
+
+const tracking = new Tracking();
+tracking.clickTokens("url");
+const pipeline = new Pipeline(client, pipelineName);
+
+values.listen(changeEvent, (changes, set) => {
+  if (!changes.page) {
+    set({ page: "1" });
+  }
+});
+
+const CustomResult = ({ values, token, onResultClicked }) =>
+  <div>
+    <h1>
+      {values.title}
+    </h1>
+    <p>
+      {values.description}
+    </p>
+  </div>;
+
+const App = () =>
+  <div className="searchApp">
+    <AutocompleteInput
+      values={values}
+      pipeline={pipeline}
+      tracking={tracking}
+    />
+    <Response pipeline={pipeline}>
+      <Summary values={values} pipeline={pipeline} tracking={tracking} />
+      <Results pipeline={pipeline} ResultRenderer={CustomResult} />
+      <Paginator values={values} pipeline={pipeline} tracking={tracking} />
+    </Response>
+  </div>;
+
+export default App;
