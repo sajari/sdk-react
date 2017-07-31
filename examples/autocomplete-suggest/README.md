922615ca2f10ea198d2d03d13d50523165be0864
David Howden <dhowden@gmail.com>
2017-07-31 10:19:09 +1000

    examples: added autocomplete-suggest example

---
+++ examples/autocomplete-suggest/README.md
@@ -0,0 +1,34 @@
+# Example: Autocomplete with suggestions
+
+This directory contains the autocomplete + suggestions UI example.
+
+![autocomplete](https://media.giphy.com/media/26zyVB5UcumOfOInu/giphy.gif)
+
+It has been created using the [Create React App](https://github.com/facebookincubator/create-react-app) tool to make development and deployment quick and easy.
+
+## Requirements
+
+**You’ll need to have Node >= 6 on your machine to develop + build prod assets for this example**. You can use [nvm](https://github.com/creationix/nvm#installation) to easily switch Node versions between different projects.
+
+# Getting Started
+
+To use this as a starting point for your own search interface, copy this entire directory into your own workspace.
+
+## Development
+
+This example comes with great dev tooling (courtousy of [Create React App](https://github.com/facebookincubator/create-react-app).  To get up and running:
+
+```shell
+$ npm install   # Install dependencies
+$ npm run start # Start development server
+```
+
+Now open [localhost:3000](localhost:3000) to see the example running in development mode.  Any changes you make to the code will be automatically synced to your browser.
+
+## Build for production
+
+To create a production-ready build of a completed application (with compressed assets etc):
+
+```shell
+$ npm build
+```
---
+++ examples/autocomplete-suggest/src/App.js
@@ -0,0 +1,148 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import {
+  Pipeline,
+  Values,
+  responseUpdatedEvent
+} from "sajari-react/controllers";
+import { AutocompleteInput } from "sajari-react/ui/text";
+
+import { Client, Tracking } from "sajari";
+
+import "sajari-react/ui/text/AutocompleteInput.css";
+
+import "./AutocompleteSuggestions.css";
+
+const project = "sajariptyltd";
+const collection = "sajari-com";
+const pipelineName = "autocomplete";
+
+const client = new Client(project, collection);
+const tracking = new Tracking();
+const pipeline = new Pipeline(client, pipelineName, tracking);
+const values = new Values();
+
+const AutocompleteSuggestion = ({ text, suggestion, setText }) => {
+  let prefix = null;
+  let prefixLen = 0;
+  if (suggestion.substr(0, text.length) === text) {
+    prefix = (
+      <strong>
+        {text}
+      </strong>
+    );
+    prefixLen = text.length;
+  }
+
+  return (
+    <div className="suggestion" onClick={() => setText(suggestion)}>
+      {prefix}
+      {suggestion.substr(prefixLen)}
+    </div>
+  );
+};
+
+class AutocompleteSuggestions extends React.Component {
+  /**
+   * propTypes
+   * @property {Values} values Values object.
+   * @property {Pipeline} pipeline Pipeline object.
+   * @property {Tracking} tracking Tracking object.
+   * @property {string} [qParam="q"] Search parameter.
+   * @property {string} [qOverrideParam="q.override"] Override parameter.
+   * @property {boolean} [focus=false] Whether to focus the input element.
+   */
+  static get propTypes() {
+    return {
+      values: PropTypes.instanceOf(Values).isRequired,
+      pipeline: PropTypes.instanceOf(Pipeline).isRequired,
+      tracking: PropTypes.instanceOf(Tracking),
+      qParam: PropTypes.string,
+      qOverrideParam: PropTypes.string
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = { response: this.props.pipeline.getResponse() };
+  }
+
+  componentDidMount() {
+    this.removeResponseListener = this.props.pipeline.listen(
+      responseUpdatedEvent,
+      this.responseUpdated
+    );
+  }
+
+  componentWillUnmount() {
+    this.removeResponseListener();
+  }
+
+  responseUpdated = response => {
+    this.setState({ response });
+  };
+
+  setText = (text, override = false) => {
+    const textValues = {
+      [this.props.qParam]: text,
+      [this.props.qOverrideParam]: override ? "true" : undefined
+    };
+    this.props.values.set(textValues);
+    if (textValues[this.props.qParam]) {
+      this.props.pipeline.search(this.props.values, this.props.tracking);
+    } else {
+      this.props.pipeline.clearResponse();
+    }
+  };
+
+  render() {
+    const { response } = this.state;
+    if (response.isEmpty()) {
+      return null;
+    }
+
+    if (response.isError()) {
+      return (
+        <div className="suggestions">
+          There was an error running the request.
+        </div>
+      );
+    }
+
+    const values = response.getValues();
+    const suggestions = values["q.suggestions"];
+    if (!suggestions) {
+      return null;
+    }
+
+    return (
+      <div className="suggestions">
+        {suggestions
+          .split(",")
+          .map(x =>
+            <AutocompleteSuggestion
+              key={x}
+              suggestion={x}
+              text={response.getQueryValues()["q"]}
+              setText={this.setText}
+            />
+          )}
+      </div>
+    );
+  }
+}
+
+AutocompleteSuggestions.defaultProps = {
+  qParam: "q",
+  qOverrideParam: "q.override"
+};
+
+const App = () =>
+  <div className="searchApp">
+    <AutocompleteInput values={values} pipeline={pipeline} />
+    <AutocompleteSuggestions values={values} pipeline={pipeline} />
+  </div>;
+
+export default App;
