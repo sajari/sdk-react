15ac88da74d0241a62ecfebedfcca1422e4f7ab5
David Howden <dhowden@gmail.com>
2017-08-25 08:45:25 +0100

    Revert "Merge pull request #70 from sajari/add-autocomplete-dropdown"
    This reverts commit fa63a786aa8079cc3f6fc2ee4ebfd735cbc914e2, reversing
    changes made to 57ebca0448de42d40dba9befb4e344af0eb4abd3.

--- src/ui/text/index.js
+++ src/ui/text/index.js
@@ -1,3 +1,2 @@
 export { default as AutocompleteInput } from "./AutocompleteInput";
-export { default as AutocompleteDropdown } from "./AutocompleteDropdown";
 export { default as Input } from "./Input";
