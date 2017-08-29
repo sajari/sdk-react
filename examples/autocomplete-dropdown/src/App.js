296683e89b17a839345e5090aba7dd8b4d96a06b
Trent Billington <pudgepacket@gmail.com>
2017-08-29 16:23:27 +1000

    example: use better variable names

--- examples/autocomplete-dropdown/src/App.js
+++ examples/autocomplete-dropdown/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Pipeline, Values } from "sajari-react/controllers";
 import { Response, Results, Summary, Paginator } from "sajari-react/ui/results";
-import AutocompleteDropdown from "sajari-react/ui/text/AutocompleteDropdown";
+import { AutocompleteDropdown } from "sajari-react/ui/text";
 
 import "sajari-react/ui/text/AutocompleteInput.css";
 import "sajari-react/ui/text/AutocompleteDropdown.css";
@@ -11,34 +11,34 @@ import "sajari-react/ui/results/Paginator.css";
 
 const project = "sajariptyltd";
 const collection = "sajari-com";
-const websitePipeline = new Pipeline(project, collection, "website");
-const websiteValues = new Values();
+const pipeline = new Pipeline(project, collection, "website");
+const values = new Values();
 
 class App extends React.Component {
   submit = query => {
     if (query) {
-      websiteValues.set({ q: query, "q.override": true });
-      websitePipeline.search(websiteValues.get());
+      values.set({ q: query, "q.override": true });
+      pipeline.search(values.get());
       return;
     }
-    websitePipeline.clearResponse();
+    pipeline.clearResponse();
   };
 
   render() {
     return (
       <div className="search-app">
         <AutocompleteDropdown
-          values={websiteValues}
-          pipeline={websitePipeline}
+          values={values}
+          pipeline={pipeline}
           numSuggestions={5}
           handleForceSearch={this.submit}
           autocompleteOnQueryChanged={true}
           showInlineCompletion={true}
         />
-        <Response pipeline={websitePipeline}>
-          <Summary values={websiteValues} pipeline={websitePipeline} />
-          <Results pipeline={websitePipeline} />
-          <Paginator values={websiteValues} pipeline={websitePipeline} />
+        <Response pipeline={pipeline}>
+          <Summary values={values} pipeline={pipeline} />
+          <Results pipeline={pipeline} />
+          <Paginator values={values} pipeline={pipeline} />
         </Response>
       </div>
     );
