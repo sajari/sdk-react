6da1b99f4aef74f73e7dac598f51ebc4aa69b5e4
Trent Billington <pudgepacket@gmail.com>
2017-09-04 18:07:31 +1000

    ui: add AutocompleteSuggestions

--- src/ui/text/index.js
+++ src/ui/text/index.js
@@ -1,4 +1,5 @@
 export { default as AutocompleteInput } from "./AutocompleteInput";
 export { default as AutocompleteDropdown } from "./AutocompleteDropdown";
 export { default as AutocompleteSuggestion } from "./AutocompleteSuggestion";
+export { default as AutocompleteSuggestions } from "./AutocompleteSuggestions";
 export { default as Input } from "./Input";
