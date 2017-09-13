1d6a7308d5673c61df0968445265b09c4a937fdf
Trent Billington <pudgepacket@gmail.com>
2017-09-13 14:07:45 +1000

    ui: refactor out autocomplete renderer and common logic

---
+++ src/ui/text/AutocompleteDropdownCommon.js
@@ -0,0 +1,200 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import {
+  Pipeline,
+  Values,
+  responseUpdatedEvent,
+  valuesUpdatedEvent
+} from "../../controllers";
+
+import AutocompleteDropdownRenderer from "./AutocompleteDropdownRenderer";
+
+const getState = (values, pipeline, qParam, numSuggestions) => {
+  const text = values.get()[qParam] || "";
+  if (!text) {
+    return {
+      text,
+      displayText: text,
+      suggestions: [],
+      selectedIndex: -1
+    };
+  }
+  const responseValues = pipeline.getResponse().getValues();
+  const suggestions = responseValues
+    ? (responseValues["q.suggestions"] || "")
+        .split(",")
+        .filter(s => s.length > 0)
+        .slice(0, numSuggestions)
+    : [];
+  return { text, displayText: text, suggestions, selectedIndex: -1 };
+};
+
+class AutocompleteDropdownCommon extends React.Component {
+  /**
+   * propTypes
+   * @property {Values} values Values object.
+   * @property {Pipeline} pipeline Pipeline object.
+   * @property {Values} forceSearchValues Values to use for forced search.
+   * @property {Pipeline} forceSearchPipeline Pipeline to use for forced search.
+   * @property {string} placeholder Placeholder to use for the input element.
+   * @property {number} [numSuggestions=5] Maximum number of suggestion to show.
+   * @property {string} [qParam="q"] Search parameter.
+   * @property {string} [qOverrideParam="q.override"] Search override parameter.
+   * @property {boolean} [autoFocus=false] Whether to focus the input element on creation.
+   */
+  static get propTypes() {
+    return {
+      values: PropTypes.instanceOf(Values).isRequired,
+      pipeline: PropTypes.instanceOf(Pipeline).isRequired,
+      forceSearchValues: PropTypes.instanceOf(Values),
+      forceSearchPipeline: PropTypes.instanceOf(Pipeline),
+      placeholder: PropTypes.string,
+      numSuggestions: PropTypes.number,
+      qParam: PropTypes.string,
+      qOverrideParam: PropTypes.string,
+      autoFocus: PropTypes.bool
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = getState(
+      props.values,
+      props.pipeline,
+      props.qParam,
+      props.numSuggestions
+    );
+
+    this.removeValuesListener = props.values.listen(
+      valuesUpdatedEvent,
+      this.valuesChanged
+    );
+    this.removeResponseListener = props.pipeline.listen(
+      responseUpdatedEvent,
+      this.valuesChanged
+    );
+  }
+
+  componentWillUpdate(nextProps) {
+    const { values: newValues, pipeline: newPipeline } = nextProps;
+    const { values: oldValues, pipeline: oldPipeline } = this.props;
+    if (newValues !== oldValues) {
+      this.removeValuesListener();
+      this.removeValuesListener = newValues.listen(
+        valuesUpdatedEvent,
+        this.valuesChanged
+      );
+    }
+    if (newPipeline !== oldPipeline) {
+      this.removeResponseListener();
+      this.removeResponseListener = newPipeline.listen(
+        responseUpdatedEvent,
+        this.valuesChanged
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    this.removeValuesListener();
+    this.removeResponseListener();
+  }
+
+  valuesChanged = () => {
+    const { values, pipeline, qParam, numSuggestions } = this.props;
+    this.setState(getState(values, pipeline, qParam, numSuggestions));
+  };
+
+  onSubmit = () => {};
+
+  handleChange = event => {
+    const { qParam, qOverrideParam, values, pipeline } = this.props;
+    const newText = event.target.value;
+    this.setState({ text: newText, displayText: newText });
+    values.set({ [qParam]: newText, [qOverrideParam]: undefined });
+    if (newText) {
+      pipeline.search(values.get());
+    } else {
+      pipeline.clearResponse(values.get());
+    }
+  };
+
+  handleEscape = () => {
+    this.setState({ suggestions: [], selectedIndex: -1 });
+  };
+
+  handleReturn = () => {
+    const { text, selectedIndex, suggestions } = this.state;
+    if (selectedIndex >= 0) {
+      this.submit(suggestions[selectedIndex]);
+    } else {
+      this.submit(text);
+    }
+  };
+
+  handleUpArrow = () => {
+    const { text, selectedIndex, suggestions } = this.state;
+    if (selectedIndex >= 0) {
+      this.setState({
+        displayText: suggestions[selectedIndex - 1] || text,
+        selectedIndex: selectedIndex - 1
+      });
+    }
+  };
+
+  handleDownArrow = () => {
+    const { selectedIndex, suggestions } = this.state;
+    if (selectedIndex < suggestions.length - 1) {
+      this.setState({
+        displayText: suggestions[selectedIndex + 1],
+        selectedIndex: selectedIndex + 1
+      });
+    }
+  };
+
+  handleRightArrowOrTab = () => {
+    const { selectedIndex, suggestions } = this.state;
+    if (selectedIndex >= 0) {
+      const newText = suggestions[selectedIndex];
+      this.setState({
+        text: newText,
+        displayText: newText,
+        selectedIndex: -1,
+        suggestions: []
+      });
+    }
+  };
+
+  render() {
+    const { text, displayText, suggestions, selectedIndex } = this.state;
+    const { placeholder, autoFocus } = this.props;
+
+    return (
+      <AutocompleteDropdownRenderer
+        submit={this.onSubmit}
+        text={text}
+        displayText={displayText}
+        suggestions={suggestions}
+        selectedIndex={selectedIndex}
+        placeholder={placeholder}
+        autoFocus={autoFocus}
+        handleChange={this.handleChange}
+        handleEscape={this.handleEscape}
+        handleReturn={this.handleReturn}
+        handleUpArrow={this.handleUpArrow}
+        handleDownArrow={this.handleDownArrow}
+        handleRightArrow={this.handleRightArrowOrTab}
+        handleTab={this.handleRightArrowOrTab}
+      />
+    );
+  }
+}
+
+AutocompleteDropdownCommon.defaultProps = {
+  qParam: "q",
+  qOverrideParam: "q.override",
+  numSuggestions: 5
+};
+
+export default AutocompleteDropdownCommon;
