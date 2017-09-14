13362873310ef0eb66bb0924445bbb143dd2be5e
Trent Billington <pudgepacket@gmail.com>
2017-09-14 11:27:55 +1000

    ui: update AutocompleteDropdownStandalone test

--- src/ui/text/AutocompleteDropdownStandalone.test.js
+++ src/ui/text/AutocompleteDropdownStandalone.test.js
@@ -7,8 +7,13 @@ test("renders without crashing", () => {
   const div = document.createElement("div");
   const pipeline = new Pipeline("", "");
   const values = new Values();
+  const onForceSearch = () => {};
   ReactDOM.render(
-    <AutocompleteDropdownStandalone pipeline={pipeline} values={values} />,
+    <AutocompleteDropdownStandalone
+      pipeline={pipeline}
+      values={values}
+      onForceSearch={onForceSearch}
+    />,
     div
   );
 });
