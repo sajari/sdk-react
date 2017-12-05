bffae5334ea09a902fea7eed5a0f8dc539f5b456
Trent Billington <pudgepacket@gmail.com>
2017-12-05 16:22:56 +1100

    results: present a friendly error message on 403

--- src/ui/results/Results.js
+++ src/ui/results/Results.js
@@ -68,7 +68,14 @@ Results.defaultProps = {
 
 const ResultsRenderer = ({ response, resultClicked, ResultRenderer }) => {
   if (response.isError()) {
-    return <div className="sj-result-error">{response.getError()}</div>;
+    if (response.getError().code === 403) {
+      return (
+        <div className="sj-result-error">
+          Authorisation for this request failed. Check your credentials.
+        </div>
+      );
+    }
+    return <div className="sj-result-error">{response.getError().message}</div>;
   }
 
   if (response.isEmpty()) {
