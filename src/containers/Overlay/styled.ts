fc214dc5f1c7c5ea6d2b2f32e87def1573818d47
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-05-30 12:37:55 +1000

    containers: add Overlay container

---
+++ src/containers/Overlay/styled.ts
@@ -0,0 +1,28 @@
+import * as React from "react";
+import {
+  override,
+  styled,
+  StyledComponent,
+  StyledProps,
+  Theme
+} from "../../components/styles";
+
+export const BlurContainer = styled("div")({
+  alignItems: "center",
+  backgroundColor: "rgba(0,0,0,0.5)",
+  bottom: 0,
+  display: "flex",
+  justifyContent: "center",
+  left: 0,
+  position: "absolute",
+  right: 0,
+  top: 0
+});
+
+export const Container = styled("div")({
+  backgroundColor: "#fff",
+  boxSizing: "border-box",
+  height: "80%",
+  padding: "1rem",
+  width: "80%"
+});
