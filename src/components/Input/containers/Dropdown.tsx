4fe28e1fc83e31a209af69457d0500a6efec72e3
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-14 14:13:10 +1000

    components/Input: add scroll listener to Dropdown container

--- src/components/Input/containers/Dropdown.tsx
+++ src/components/Input/containers/Dropdown.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { css } from "emotion";
-import { StyledProps, styled } from "../../styles";
+import * as React from "react";
+import { styled, StyledProps } from "../../styles";
 
 import { Resizer } from "./Resizer";
 
@@ -9,17 +9,63 @@ export interface DropdownProps {
   element?: HTMLElement;
 }
 
-export class Dropdown extends React.Component<DropdownProps> {
-  render() {
+export interface DropdownState {
+  top: number;
+}
+
+export class Dropdown extends React.Component<DropdownProps, DropdownState> {
+  public state = { top: -1 };
+
+  public componentDidMount() {
+    document.addEventListener("scroll", this.scrollListener, true);
+    this.handleScrollEvent();
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener("scroll", this.scrollListener, true);
+  }
+
+  public render() {
     const { isOpen, element, children } = this.props;
+    const { top } = this.state;
     return (
       <Resizer element={element}>
         {({ offset }) =>
-          isOpen ? <Container position={offset}>{children}</Container> : null
+          isOpen ? (
+            <Container
+              position={{ ...offset, top: top === -1 ? offset.top : top }}
+            >
+              {children}
+            </Container>
+          ) : null
         }
       </Resizer>
     );
   }
+
+  private ticking = false;
+  private scrollListener = (event: any) => {
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        this.handleScrollEvent();
+        this.ticking = false;
+      });
+      this.ticking = true;
+    }
+  };
+
+  private handleScrollEvent = () => {
+    const { element } = this.props;
+    if (element === undefined) {
+      return;
+    }
+
+    const { top } = element.getBoundingClientRect();
+    if (Math.sign(top) === -1) {
+      return;
+    }
+    this.setState(state => ({ ...state, top }));
+  };
 }
 
 export interface ContainerProps extends StyledProps<HTMLDivElement> {
