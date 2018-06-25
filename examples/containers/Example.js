00e8501b9940ac46df730832d3c52a0f51997341
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-25 12:56:07 +1000

    dev:examples: updates

--- examples/containers/Example.js
+++ examples/containers/Example.js
@@ -1,19 +1,5 @@
 import React, { Component } from "react";
-import CodeSandboxer from "react-codesandboxer";
 import styled from "react-emotion";
-
-const AddToCodesandbox = ({ example }) => (
-  <CodeSandboxer
-    examplePath={example}
-    gitInfo={{
-      account: "sajari",
-      repository: "sajari-react",
-      host: "github"
-    }}
-  >
-    {() => <button type="submit">Upload to CodeSandbox</button>}
-  </CodeSandboxer>
-);
 
 export const Example = ({ children }) => (
   <Container>
