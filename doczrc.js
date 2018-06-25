32f08e7331d45e742e6b478089a1ac978d6b3edc
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-25 13:05:21 +1000

    docs: add docz config

---
+++ doczrc.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Sajari React SDK",
+  source: "./src",
+  typescript: true,
+  wrapper: "src/docs/wrapper.tsx",
+  port: 5000,
+  themeConfig: {
+    colors: {
+      primary: "#459CE7"
+    },
+    styles: {
+      h1: {
+        fontSize: "5rem"
+      }
+    }
+  }
+};
