cd445aa463f14eef0e4867b680a9a4a447312085
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-06-26 15:34:39 +1000

    docs: update facets & filtering docs

---
+++ src/docs/helpers.ts
@@ -0,0 +1,30 @@
+import { Filter } from "../controllers";
+import { styled, StyledComponent } from "../components/styles";
+
+export const ExampleContainer = styled("div")({
+  border: "1px solid #D3DCE6",
+  borderRadius: "5px",
+  padding: "2rem",
+  marginBottom: "1rem"
+});
+
+export const categoryFilter = () =>
+  new Filter(
+    {
+      All: "",
+      Blog: "dir1='blog'",
+      Articles: "dir1='articles'"
+    },
+    ["All"]
+  );
+
+export const categoryMultiFilter = () =>
+  new Filter(
+    {
+      All: "",
+      Blog: "dir1='blog'",
+      Articles: "dir1='articles'"
+    },
+    ["Blog", "Articles"],
+    true
+  );
