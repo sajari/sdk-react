fa0e2bc4b98762d8ef003f15c17a4bc425be5061
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-12-05 16:13:32 -0800

    components/Paginator: allow for custom component renderers to be provided

---
+++ src/components/Paginator/Container.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+import { Consumer } from "../context";
+import { PaginateFn } from "../context/pipeline/context";
+
+export interface PaginatorContainerProps {
+  children?: PaginatorContainerRenderFn;
+}
+
+export interface PaginatorContainerRenderFnArgs {
+  page: number;
+  resultsPerPage: number;
+  totalResults: number;
+  totalPages: number;
+  paginate: PaginateFn;
+}
+
+export interface PaginatorContainerRenderFn {
+  (args: PaginatorContainerRenderFnArgs): React.ReactNode;
+}
+
+export function PaginatorContainer(props: PaginatorContainerProps) {
+  return (
+    <Consumer>
+      {({ search: { response, config }, paginate }) => {
+        if (
+          response === null ||
+          response === undefined ||
+          response.isEmpty() ||
+          response.isError()
+        ) {
+          return null;
+        }
+
+        const queryValues = response.getQueryValues();
+        if (queryValues === undefined) {
+          return;
+        }
+
+        const page = queryValues.get(config.pageParam)
+          ? parseInt(queryValues.get(config.pageParam) as string, 10)
+          : 1;
+        const resultsPerPage = queryValues.get(config.resultsPerPageParam)
+          ? parseInt(queryValues.get(config.resultsPerPageParam) as string, 10)
+          : 10;
+        const totalResults = response.getTotalResults();
+        if (totalResults === undefined) {
+          return;
+        }
+
+        if (totalResults <= resultsPerPage) {
+          return null;
+        }
+
+        const totalPages = Math.ceil(totalResults / resultsPerPage);
+        if (totalPages === 0) {
+          return null;
+        }
+
+        if (props.children == null) {
+          return null;
+        }
+
+        return props.children({
+          page,
+          resultsPerPage,
+          totalResults,
+          totalPages,
+          paginate
+        });
+      }}
+    </Consumer>
+  );
+}
--- src/components/Paginator/Paginator.tsx
+++ src/components/Paginator/Paginator.tsx
@@ -1,12 +1,12 @@
 import { css, cx } from "emotion";
 import * as React from "react";
 
-import { Consumer } from "../context";
 import { PaginateFn } from "../context/pipeline/context";
 import { pageNumbers } from "./utils";
 
 import { LeftChevron, RightChevron } from "./icons";
 import { Container, PageButton, PageNumber } from "./styled";
+import { PaginatorContainer } from "./Container";
 
 export interface PaginatorProps {
   className?: string;
@@ -15,139 +15,153 @@ export interface PaginatorProps {
     controls?: React.CSSProperties;
     number?: (isCurrent: boolean) => React.CSSProperties;
   };
+
+  PreviousButtonRenderer?: React.ComponentType<PageButtonProps>;
+  NextButtonRenderer?: React.ComponentType<PageButtonProps>;
+  PageNumberRenderer?: React.ComponentType<PageNumberProps>;
 }
 
-export class Paginator extends React.Component<PaginatorProps> {
-  public render() {
-    const { styles = {} } = this.props;
-    return (
-      <Consumer>
-        {({ search: { response, config }, paginate }) => {
-          if (
-            response === null ||
-            response === undefined ||
-            response.isEmpty() ||
-            response.isError()
-          ) {
-            return null;
-          }
-
-          const queryValues = response.getQueryValues();
-          if (queryValues === undefined) {
-            return;
-          }
-
-          const page = queryValues.get(config.pageParam)
-            ? parseInt(queryValues.get(config.pageParam) as string, 10)
-            : 1;
-          const resultsPerPage = queryValues.get(config.resultsPerPageParam)
-            ? parseInt(
-                queryValues.get(config.resultsPerPageParam) as string,
-                10
-              )
-            : 10;
-          const totalResults = response.getTotalResults();
-          if (totalResults === undefined) {
-            return;
-          }
-
-          if (totalResults <= resultsPerPage) {
-            return null;
-          }
-
-          const totalPages = Math.ceil(totalResults / resultsPerPage);
-          if (totalPages === 0) {
-            return null;
-          }
-
-          return (
-            <Container
-              className={cx("sj-paginator", this.props.className)}
-              aria-label="Pagination Navigation"
-              styles={styles.container}
+export function Paginator(props: PaginatorProps) {
+  const {
+    styles = {},
+    PreviousButtonRenderer = PreviousPageButton,
+    NextButtonRenderer = NextPageButton,
+    PageNumberRenderer = DefaultPageNumber
+  } = props;
+  return (
+    <PaginatorContainer>
+      {({ page, totalPages, paginate }) => {
+        return (
+          <Container
+            className={cx("sj-paginator", props.className)}
+            aria-label="Pagination Navigation"
+            styles={styles.container}
+          >
+            {page !== 1 && (
+              <PreviousButtonRenderer
+                isDisabled={page === 1}
+                onClick={prevPage(paginate, page)}
+                styles={styles.controls}
+              />
+            )}
+            <ul
+              className={css({
+                display: "inline-flex",
+                listStyle: "none",
+                margin: 0,
+                padding: 0
+              })}
             >
-              {page !== 1 && (
-                <PageButton
-                  isDisabled={page === 1}
-                  onClick={this.prevPage(paginate, page)}
-                  aria-label="Goto Previous Page"
-                  className="sj-paginator__page-button"
-                  styles={styles.controls}
-                >
-                  <LeftChevron />
-                </PageButton>
-              )}
-              <ul
-                className={css({
-                  display: "inline-flex",
-                  listStyle: "none",
-                  margin: 0,
-                  padding: 0
-                })}
-              >
-                {pageNumbers(page, totalPages).map((pageNumber: number) => (
-                  <li key={pageNumber}>
-                    <PageNumber
-                      className={cx(
-                        "sj-paginator__page-number",
-                        page === pageNumber &&
-                          "sj-paginator__page-number--current",
-                        styles.number &&
-                          css(styles.number(page === pageNumber) as any)
-                      )}
-                      isCurrent={page === pageNumber}
-                      onClick={this.setPage(paginate, pageNumber)}
-                      aria-label={
-                        page === pageNumber
-                          ? `Current Page, Page ${pageNumber}`
-                          : `Page ${pageNumber}`
-                      }
-                      aria-current={page === pageNumber ? true : undefined}
-                    >
-                      {pageNumber}
-                    </PageNumber>
-                  </li>
-                ))}
-              </ul>
-              {page !== totalPages && (
-                <PageButton
-                  isDisabled={page === totalPages}
-                  onClick={this.nextPage(paginate, page, totalPages)}
-                  aria-label="Goto Next Page"
-                  className="sj-paginator__page-button"
-                  styles={styles.controls}
-                >
-                  <RightChevron />
-                </PageButton>
-              )}
-            </Container>
-          );
-        }}
-      </Consumer>
-    );
-  }
+              {pageNumbers(page, totalPages).map((pageNumber: number) => (
+                <li key={pageNumber}>
+                  <PageNumberRenderer
+                    pageNumber={pageNumber}
+                    isCurrent={page === pageNumber}
+                    onClick={setPage(paginate, pageNumber)}
+                    styles={styles.number}
+                  />
+                </li>
+              ))}
+            </ul>
+            {page !== totalPages && (
+              <NextButtonRenderer
+                isDisabled={page === totalPages}
+                onClick={nextPage(paginate, page, totalPages)}
+                styles={styles.controls}
+              />
+            )}
+          </Container>
+        );
+      }}
+    </PaginatorContainer>
+  );
+}
 
-  private prevPage = (paginate: PaginateFn, page: number) => () => {
+interface PageButtonProps {
+  isDisabled: boolean;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  styles?: React.CSSProperties;
+}
+
+function PreviousPageButton(props: PageButtonProps) {
+  return (
+    <PageButton
+      isDisabled={props.isDisabled}
+      onClick={props.onClick}
+      aria-label="Goto Previous Page"
+      className="sj-paginator__page-button"
+      styles={props.styles}
+    >
+      <LeftChevron />
+    </PageButton>
+  );
+}
+
+function NextPageButton(props: PageButtonProps) {
+  return (
+    <PageButton
+      isDisabled={props.isDisabled}
+      onClick={props.onClick}
+      aria-label="Goto Next Page"
+      className="sj-paginator__page-button"
+      styles={props.styles}
+    >
+      <RightChevron />
+    </PageButton>
+  );
+}
+
+export interface PageNumberProps {
+  pageNumber: number;
+  onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+  isCurrent: boolean;
+  styles?: (isCurrent: boolean) => React.CSSProperties;
+}
+
+function DefaultPageNumber(props: PageNumberProps) {
+  return (
+    <PageNumber
+      className={cx(
+        "sj-paginator__page-number",
+        props.isCurrent && "sj-paginator__page-number--current",
+        props.styles && css(props.styles(props.isCurrent) as any)
+      )}
+      isCurrent={props.isCurrent}
+      onClick={props.onClick}
+      aria-label={
+        props.isCurrent
+          ? `Current Page, Page ${props.pageNumber}`
+          : `Page ${props.pageNumber}`
+      }
+      aria-current={props.isCurrent ? true : undefined}
+    >
+      {props.pageNumber}
+    </PageNumber>
+  );
+}
+
+function prevPage(paginate: PaginateFn, page: number) {
+  return function closure() {
     if (page === 1) {
       return;
     }
 
-    this.setPage(paginate, page - 1)();
+    setPage(paginate, page - 1)();
   };
+}
 
-  private nextPage = (
-    paginate: PaginateFn,
-    page: number,
-    totalPages: number
-  ) => () => {
+function nextPage(paginate: PaginateFn, page: number, totalPages: number) {
+  return function closure() {
     if (page === totalPages) {
       return;
     }
 
-    this.setPage(paginate, page + 1)();
+    setPage(paginate, page + 1)();
   };
+}
 
-  private setPage = (paginate: PaginateFn, page: number) => () => {
+function setPage(paginate: PaginateFn, page: number) {
+  return function closure() {
     window.scrollTo(0, 0);
     paginate(page);
   };
