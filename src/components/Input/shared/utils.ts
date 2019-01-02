8e4be40805982c276e11a72ae1a1fa613b5bcdbd
zlatan <zlatan.pham@gmail.com>
2019-01-02 21:22:54 +0700

    update Input based on Search render-prop

--- src/components/Input/shared/utils.ts
+++ src/components/Input/shared/utils.ts
@@ -1,13 +1,3 @@
-export const isNotEmptyString = (a: string, b: string) => (a === "" ? b : a);
-
-export function isNotEmptyArray<T>(a: T, b: T): T {
-  if (!Array.isArray(a) || !Array.isArray(b)) {
-    throw new Error("provided arguments must be arrays");
-  }
-
-  return a.length === 0 ? b : a;
-}
-
 export function trimPrefix(str: string, prefix: string) {
   if (str === undefined || prefix === "") {
     return "";
