7ea3ed5e360c0bf23d8a11558ce0aa3c658ed29a
zlatan <zlatan.pham@gmail.com>
2020-04-20 21:53:37 +0700

    update styles based on new design

--- src/components/RangeSlider/styled.tsx
+++ src/components/RangeSlider/styled.tsx
@@ -8,6 +8,7 @@ const BaseStyledRange = styled(Range)`
     padding: 5px 0;
     height: 4px;
     width: 100%;
+    margin:40px 0 14px 0;
     touch-action: none;
     box-sizing: border-box;
   }
@@ -17,7 +18,7 @@ const BaseStyledRange = styled(Range)`
   .rc-slider-rail {
     position: absolute;
     width: 100%;
-    background-color: #e9e9e9;
+    background-color: #DADFE7;
     height: 4px;
     border-radius: 6px;
   }
@@ -26,27 +27,15 @@ const BaseStyledRange = styled(Range)`
     left: 0;
     height: 4px;
     border-radius: 6px;
-    background-color: #abe2fb;
-  }
-  .rc-slider-handle-dragging.rc-slider-handle-dragging.rc-slider-handle-dragging {
-    border-color: #ddd;
-    box-shadow: 0 0 0 5px #ddd;
-  }
-  .rc-slider-handle:focus {
-    outline: none;
   }
   .rc-slider-handle-click-focused:focus {
-    border-color: #ddd;
+    border-color: #D9DBFD;
     box-shadow: unset;
   }
   .rc-slider-handle:hover {
-    border-color: #ddd;
+    border-color: #D9DBFD;
   }
-  .rc-slider-handle:active {
-    border-color: #ddd;
-    box-shadow: 0 0 5px #ddd;
-    cursor: grabbing;
-  }
+
   .rc-slider-handle {
     position: absolute;
     width: 14px;
@@ -54,33 +43,56 @@ const BaseStyledRange = styled(Range)`
     margin-top: -5px;
     cursor: grab;
     border-radius: 50%;
-    border: solid 2px #ddd;
     background-color: #fff;
     touch-action: pan-x;
     border: none;
     background-color: transparent;
 
-    &:after {
-      box-shadow: 0 16px 42px 0 rgba(0, 0, 0, 0.16);
-      display: block;
+    &:before,
+    &:after{
       content: "";
-      width: 16px;
-      height: 16px;
+      display: block;
+      width:100%;
+      height:100%;
       position: absolute;
       left: 50%;
-      top: 50%;
       transform: translate(-50%, -50%);
+      top: 50%;
       border-radius: 999px;
-      background-color: #232323;
+      box-sizing:content-box;
+    }
+
+
+    &:after {
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.16);
+      border:1px solid #DADFE7;
+      background-color: #ffffff;
+    }
+
+    &:before{
+      opacity:0;
+      border:5px solid ${props => props.theme.colors?.brand || '#6772F9'};
+    }
+  }
+
+  .rc-slider-handle:focus{
+    outline:none;
+  }
+
+  .rc-slider-handle:focus,
+  .rc-slider-handle:active,
+  .rc-slider-handle-dragging.rc-slider-handle-dragging.rc-slider-handle-dragging {
+    cursor: grabbing;
+    &:before{
+      opacity:0.2;
+    }
+    &:after{
+     border-color: ${props => props.theme.colors?.brand || '#6772F9'};
     }
   }
 
   .rc-slider-track {
-    background-color: #232323;
-  }
-
-  &:hover {
-    opacity: 1;
+    background-color: ${props => props.theme.colors?.brand || '#6772F9'};
   }
 `;
 
