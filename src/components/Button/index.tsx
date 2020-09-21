63ed61be47e3f59e228b35c6548117d95ea4845c
zlatan <zlatan.pham@gmail.com>
2020-09-21 20:18:24 +0700

    move Button to one index file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,1 +1,13 @@
-export * from "./Button";
+import React, { HTMLAttributes } from "react";
+import tw, { styled } from "twin.macro";
+
+const StyledButton = styled.button`
+  ${tw`px-5 py-2 border-none text-white`};
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
+const Button = (props: HTMLAttributes<HTMLButtonElement>) => {
+  return <StyledButton {...props} />;
+};
+
+export default Button;
