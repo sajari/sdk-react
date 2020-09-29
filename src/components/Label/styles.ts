3a1932ebf770667dff1271f0b8d51901210f7d3c
zlatan <zlatan.pham@gmail.com>
2020-09-29 21:38:53 +0700

    add label component

---
+++ src/components/Label/styles.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/core";
+import tw from "twin.macro";
+import { LabelProps } from ".";
+
+export default function useLabelStyles({ visuallyHidden }: LabelProps) {
+  const styles = [];
+
+  if (visuallyHidden) {
+    styles.push(tw`sr-only`);
+  } else {
+    styles.push(tw`inline-flex items-center cursor-pointer`);
+  }
+
+  return css(styles);
+}
