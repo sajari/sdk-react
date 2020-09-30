1f4754622b9a2b11604f48ffadc4ec12ec7a97a8
zlatan <zlatan.pham@gmail.com>
2020-09-30 16:14:51 +0700

    add radio and radioGroup component

---
+++ src/components/Radio/types.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface Props {
+  /** The state when entering an invalid input */
+  invalid?: boolean;
+}
+
+export interface RadioProps
+  extends Props,
+    React.InputHTMLAttributes<HTMLInputElement> {}
