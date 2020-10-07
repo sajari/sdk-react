f57fedfb47492182f349bbe66aa8b8c5995eabf7
Thanh Pham <zlatan.pham@gmail.com>
2020-10-08 00:19:30 +0700

    add label component (#153)

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,20 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React from 'react';
+import { __DEV__ } from '../../utils/assertion';
+import useLabelStyles from './styles';
+import { LabelProps } from './types';
+
+
+const Label = React.forwardRef((props: LabelProps, ref?: React.Ref<HTMLLabelElement>) => {
+  const styles = useLabelStyles(props)
+
+  return <label {...props} ref={ref} css={styles} />;
+});
+
+if (__DEV__) {
+  Label.displayName = 'Label';
+}
+
+export default Label;
+export type { LabelProps };
