a7fbccf3acf6c521a5c6acd00e6b263098138efd
zlatan <zlatan.pham@gmail.com>
2020-10-08 00:45:46 +0700

    prettify

--- src/components/CheckboxGroup/index.tsx
+++ src/components/CheckboxGroup/index.tsx
@@ -1,11 +1,11 @@
 /** @jsx jsx */
-import { css,jsx } from "@emotion/core";
-import { useId } from '@reach/auto-id';
-import React, { cloneElement, useRef, useState } from 'react';
-import tw from 'twin.macro'
+import { css, jsx } from "@emotion/core";
+import { useId } from "@reach/auto-id";
+import React, { cloneElement, useRef, useState } from "react";
+import tw from "twin.macro";
 import useSpacing from "../../hooks/use-spacing";
-import { cleanChildren } from '../../utils/react-helpers';
-import { CheckboxGroupProps } from './types';
+import { cleanChildren } from "../../utils/react-helpers";
+import { CheckboxGroupProps } from "./types";
 
 const CheckboxGroup = ({
   onChange,
@@ -13,14 +13,14 @@ const CheckboxGroup = ({
   defaultValue,
   inline,
   value: valueProp,
-  spacing = inline ? '4' : '2',
+  spacing = inline ? "4" : "2",
   children,
   ...rest
 }: CheckboxGroupProps) => {
   const [values, setValues] = useState(defaultValue || []);
   const { current: isControlled } = useRef(valueProp != null);
   const internalValues = (isControlled ? valueProp : values) || [];
-  const spacingStyles = useSpacing({spacing, inline})
+  const spacingStyles = useSpacing({ spacing, inline });
 
   const internalChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { checked, value } = event.target;
@@ -28,7 +28,7 @@ const CheckboxGroup = ({
     if (checked) {
       newValues = [...internalValues, value];
     } else {
-      newValues = internalValues.filter((val) => val !== value);
+      newValues = internalValues.filter(val => val !== value);
     }
     if (!isControlled) {
       setValues(newValues);
@@ -47,12 +47,16 @@ const CheckboxGroup = ({
     return cloneElement(child, {
       name: `${internalName}-${index}`,
       onChange: internalChange,
-      checked: internalValues.includes(child.props.value),
+      checked: internalValues.includes(child.props.value)
     });
   });
 
   return (
-    <div {...rest} role="group" css={css([inline ? tw`flex-row` : tw`flex-col`, spacingStyles])}>
+    <div
+      {...rest}
+      role="group"
+      css={css([inline ? tw`flex-row` : tw`flex-col`, spacingStyles])}
+    >
       {clones}
     </div>
   );
