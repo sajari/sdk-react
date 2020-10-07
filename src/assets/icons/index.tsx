9c6060e64a5bb0c1944b771f76e8dbadd0973aed
Tuan Dao <tdao@sajari.com>
2020-10-07 11:35:59 +0700

    Tidy up the icon exports

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,8 +1,6 @@
-import { ReactComponent as MicIcon } from "./mic.svg";
-export { MicIcon };
 import { ReactComponent as EmptyMicIcon } from "./empty-mic.svg";
-export { EmptyMicIcon };
+import { ReactComponent as MicIcon } from "./mic.svg";
 import { ReactComponent as SearchIcon } from "./search.svg";
-export { SearchIcon };
 import { ReactComponent as SpinnerIcon } from "./spinner.svg";
-export { SpinnerIcon };
+
+export { MicIcon, EmptyMicIcon, SpinnerIcon, SearchIcon };
