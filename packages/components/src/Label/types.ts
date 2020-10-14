7da60d4f9a4a2d9617cb98520133b559199af658
Sam Potts <sam@potts.es>
2020-10-14 16:31:41 +1100

    fix: revert removal of htmlFor attribute on Label

--- packages/components/src/Label/types.ts
+++ packages/components/src/Label/types.ts
@@ -3,6 +3,8 @@ import React from 'react';
 interface Props {
   /** Visually hidden label */
   visuallyHidden?: boolean;
+  /** The ID of the input associated to the label */
+  htmlFor: string;
 }
 
 export interface LabelProps extends Props, React.HTMLAttributes<HTMLLabelElement> {}
