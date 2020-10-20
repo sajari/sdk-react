5988ef3be201ad243f771410165eb86cf9d56dc2
Tuan Dao <tdao@sajari.com>
2020-10-20 13:57:27 +0700

    Move to new project structure

--- packages/components/src/RangeInput/input.tsx
+++ packages/components/src/RangeInput/input.tsx
@@ -1,16 +1,14 @@
+/* eslint-disable */
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { useTextField } from "@react-aria/textfield";
-import React from "react";
-import tw from "twin.macro";
-import { RangeInputInputProps } from "./types";
+import { jsx } from '@emotion/core';
+import { useTextField } from '@react-aria/textfield';
+import React from 'react';
+import tw from 'twin.macro';
+import { RangeInputInputProps } from './types';
 
 export const Input = (props: RangeInputInputProps) => {
   const ref = React.useRef<any>();
-  const { inputProps, labelProps } = useTextField(
-    { ...props, type: "number" },
-    ref
-  );
+  const { inputProps, labelProps } = useTextField({ ...props, type: 'number' }, ref);
   return (
     <div css={tw`flex`}>
       <label css={tw`sr-only`} {...labelProps}>
@@ -19,6 +17,8 @@ export const Input = (props: RangeInputInputProps) => {
       <input
         css={tw`block w-full text-gray-600 border border-gray-300 border-solid focus:shadow-outline-blue outline-none transition-shadow duration-200 ease-in-out rounded px-3 py-2`}
         {...inputProps}
+        min={props.min}
+        max={props.max}
         ref={ref}
       />
     </div>
