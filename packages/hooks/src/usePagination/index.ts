05e72354f9325e95ccd61a5eed43a71d221bbf2e
Tuan Dao <tdao@sajari.com>
2020-10-21 11:14:42 +0700

    feat: v3 - add usePagination hook

---
+++ packages/hooks/src/usePagination/index.ts
@@ -0,0 +1,35 @@
+import { useCallback, useMemo } from 'react';
+
+import { useContext } from '../SearchContextProvider';
+import { UsePaginationResult } from './types';
+
+function usePagination(key: 'search' | 'instant'): UsePaginationResult {
+  const context = useContext();
+  const { paginate } = context;
+  const { response, config } = context[key];
+  const queryValues = response?.getQueryValues();
+  const page = queryValues?.get(config.pageParam) ? parseInt(queryValues.get(config.pageParam) as string, 10) : 1;
+  const resultsPerPage = queryValues?.get(config.resultsPerPageParam)
+    ? parseInt(queryValues?.get(config.resultsPerPageParam) as string, 10)
+    : 10;
+  const totalResults = response?.getTotalResults() || 0;
+  const totalPages = Math.ceil(totalResults / resultsPerPage);
+  const hasNext = useMemo(() => page < totalPages, [page, totalPages]);
+  const hasPrevious = useMemo(() => page > 1, [page]);
+  const nextPage = useCallback(() => hasNext && paginate(page + 1), [paginate, hasNext]);
+  const previousPage = useCallback(() => hasPrevious && paginate(page - 1), [paginate, hasPrevious]);
+
+  return {
+    page,
+    pageCount: totalPages,
+    totalResults,
+    setPage: paginate,
+    pageSize: resultsPerPage,
+    hasNext,
+    hasPrevious,
+    nextPage,
+    previousPage,
+  };
+}
+
+export default usePagination;
