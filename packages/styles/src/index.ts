b63cc623e8176bf33b902dbf505f658307bea776
TuanD <heryhudson@gmail.com>
2020-10-22 05:23:42 +0700

    fix: minor fixes (#176)
    Co-authored-by: Tuan Dao <tdao@sajari.com>

--- packages/styles/src/index.ts
+++ packages/styles/src/index.ts
@@ -2,3 +2,4 @@ export { default as ThemeProvider } from './styled/theming';
 export { useTheme } from './styled/theming';
 export { default as styled } from './styled/styled';
 export { default as tailwindConfig } from './tailwind.config';
+export * from './utils';
---
+++ packages/styles/src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './assertion';
+export * from './object';
+export * from './string';
