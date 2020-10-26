4e5377b1fb8e13b50de18f244e5f74ce3946d5ac
Tuan Dao <tdao@sajari.com>
2020-10-26 11:53:04 +0700

    feat: v3 - add useSorting hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -2,4 +2,5 @@ export { default as SearchContextProvider } from './SearchContextProvider';
 export { default as useSearchContext } from './useSearchContext';
 export { default as usePagination } from './usePagination';
 export { default as usePageSize } from './usePageSize';
+export { default as useSorting } from './useSorting';
 export { Values, Pipeline } from './SearchContextProvider/controllers';
