384c233ac1012c52ea7500469ae544983e8da815
Sam Potts <sam@potts.es>
2020-11-10 10:40:49 +1100

    chore: fix SSR build again

--- packages/search-ui/src/Results/styles.ts
+++ packages/search-ui/src/Results/styles.ts
@@ -3,7 +3,7 @@ import tw from 'twin.macro';
 import { mapStyles } from '../utils/style-props';
 import { ResultsProps } from './types';
 
-export default function useResultsStyles({ appearance = 'list' }: ResultsProps): Styles {
+export default function useResultsStyles({ appearance = 'list' }: ResultsProps) {
   const styles = {
     container: [
       appearance === 'grid' ? tw`grid md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-8` : tw`flex flex-col space-y-8`,
