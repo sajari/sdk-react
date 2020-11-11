9b3c4423173dcb55c69f1d85687e9c6e2608b2cd
Sam Potts <sam@potts.es>
2020-11-12 10:27:45 +1100

    fix: RangeInput styling improvement

--- packages/components/src/RangeInput/components/Handle/index.tsx
+++ packages/components/src/RangeInput/components/Handle/index.tsx
@@ -3,7 +3,7 @@ import { jsx } from '@emotion/core';
 import Color from 'color';
 import React from 'react';
 import { useTheme } from 'sajari-react-sdk-styles';
-import tw from 'twin.macro';
+import tw, { theme as twTheme } from 'twin.macro';
 
 interface Props {
   active: boolean;
@@ -12,16 +12,21 @@ interface Props {
 const Fill = React.forwardRef((props: Props, ref?: React.Ref<HTMLButtonElement>) => {
   const theme = useTheme();
   const { active, ...rest } = props;
-  const color = new Color(theme.color.primary.base);
+  const colorPrimary = new Color(theme.color.primary.base);
+  const colorShadow = new Color(twTheme`colors.gray.900`);
 
   return (
     <button
       type="button"
       css={[
-        tw`w-4 h-4 transition duration-200 bg-white border border-gray-300 rounded-full shadow-sm outline-none appearance-none cursor-pointer before:(absolute transform -translate-x-1/2 text-sm pb-2 text-center transition-opacity duration-200)`,
+        tw`w-4 h-4 transition duration-200 bg-white border-none rounded-full outline-none appearance-none cursor-pointer before:(absolute transform -translate-x-1/2 text-sm pb-2 text-center transition-opacity duration-200)`,
         {
-          boxShadow: active ? `0 0 0 3px ${color.alpha(0.25).hsl().string()}` : undefined,
-          borderColor: active ? color.alpha(0.75).hsl().string() : undefined,
+          boxShadow: active
+            ? `inset 0 0 0 1px ${colorPrimary},
+              0 0 0 3px ${colorPrimary.alpha(0.25).hsl()}`
+            : `inset 0 0 0 1px ${colorShadow.alpha(0.15).hsl()},
+              inset 0 -1px 0 ${colorShadow.alpha(0.15).hsl()},
+              0 1px 1px ${colorShadow.alpha(0.1).hsl()}`,
         },
         `&::before {
           content: attr(data-value);
@@ -32,7 +37,7 @@ const Fill = React.forwardRef((props: Props, ref?: React.Ref<HTMLButtonElement>)
         &:hover::before,
         &:focus::before {
           opacity: 1;
-        }        `,
+        }`,
         { touchAction: 'pan-x' },
       ]}
       ref={ref}
