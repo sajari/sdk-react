8e39932f346c8c03c30b053279fce3b3bde7ad30
Sam Potts <sam@potts.es>
2020-11-14 23:11:39 +1100

    fix: enable neutral by default to fix currency display in Safari

--- packages/search-ui/src/utils/number.ts
+++ packages/search-ui/src/utils/number.ts
@@ -2,9 +2,10 @@
  * Format a number into a localised format
  *
  * @param input
- * @param currencyCode - The ISO currency code
+ * @param currencyCode - Optional ISO currency code (e.g. USD)
+ * @param neutral - Use neutral language (e.g. `en` rather than `en-GB`). This is often useful when presenting currencies.
  */
-export function formatNumber(input = 0, currencyCode = '', neutral = false) {
+export function formatNumber(input = 0, currencyCode = '', neutral = true) {
   if (typeof input !== 'number') {
     return input;
   }
