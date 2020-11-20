e96f44ee89d3df9f76960e197450dd888c964065
zlatan <zlatan.pham@gmail.com>
2020-11-20 10:47:41 +0700

    update to use result view from combobox

--- packages/search-ui/src/Input/index.tsx
+++ packages/search-ui/src/Input/index.tsx
@@ -1,13 +1,11 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { Combobox } from '@sajari/react-components';
-import { useAutocomplete, useQuery, useSearch, useTracking } from '@sajari/react-hooks';
+import { useAutocomplete, useQuery, useSearch } from '@sajari/react-hooks';
 import { __DEV__ } from '@sajari/react-sdk-utils';
 import { Result as ResultResp } from '@sajari/sdk-js';
 import React from 'react';
 
-import { ResultView } from '../Results/Result';
-import useInputStyles from './styles';
 import { InputProps } from './types';
 
 const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInputElement>) => {
@@ -21,10 +19,26 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
     items = suggestions;
   } else if (mode !== 'typeahead') {
     // only display 5 items
-    items = results.splice(0, 5);
-  }
+    items = results.splice(0, 5).map((result) => {
+      const {
+        values: { description, image, title, url },
+        token,
+      } = result as ResultResp;
+      let clickToken: string | undefined;
 
-  const { handleResultClicked } = useTracking();
+      if (token && 'click' in token) {
+        clickToken = token.click;
+      }
+
+      const link = clickToken || url.toString();
+      return {
+        title,
+        url: link,
+        description,
+        image,
+      };
+    });
+  }
 
   return (
     <Combobox
@@ -33,9 +47,6 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
       placeholder={placeholder}
       mode={mode}
       items={items}
-      itemToString={
-        mode === 'suggestions' ? (item: string) => item : (item: ResultResp) => item?.values.title.toString()
-      }
       completion={mode === 'typeahead' ? completion : ''}
       value={query}
       onChange={(value) => {
@@ -63,54 +74,6 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
         mode === 'suggestions'
           ? (item) => {
               search(item as string);
-            }
-          : undefined
-      }
-      itemToUrl={(item: ResultResp) => item.values.url.toString()}
-      renderItem={
-        mode === 'results'
-          ? ({ item, getItemProps, selected, index }) => {
-              const {
-                values: { category, description, image, price, rating, title, url },
-                token,
-              } = item as ResultResp;
-              const styles = useInputStyles({ selected });
-              let clickToken: string | undefined;
-              if (token && 'click' in token) {
-                clickToken = token.click;
-              }
-
-              return (
-                <div
-                  {...getItemProps({
-                    item,
-                    index,
-                    onKeyDown: (e) => {
-                      if (e.key === 'Enter' && url) {
-                        window.location.href = url.toString();
-                      }
-                    },
-                    onClick: () => {
-                      const link = clickToken || url.toString();
-                      if (link) {
-                        handleResultClicked(link);
-                        window.location.href = url.toString();
-                      }
-                    },
-                  })}
-                  css={styles.item}
-                >
-                  <ResultView
-                    title={title.toString()}
-                    url={clickToken || url.toString()}
-                    category={category?.toString()}
-                    description={description.toString()}
-                    image={image.toString()}
-                    price={price?.toString()}
-                    rating={Number(rating)}
-                  />
-                </div>
-              );
             }
           : undefined
       }
