2042bc27bc9846b3e5b8b086e6683cc08740eeb4
Sam Potts <sam@potts.es>
2020-11-26 22:34:17 +1100

    fix: hide PageSize when not required

--- packages/search-ui/src/PageSize/index.tsx
+++ packages/search-ui/src/PageSize/index.tsx
@@ -3,7 +3,7 @@
 import { jsx } from '@emotion/core';
 import { useId } from '@reach/auto-id';
 import { Label, Select } from '@sajari/react-components';
-import { usePageSize } from '@sajari/react-hooks';
+import { usePageSize, useSearchContext } from '@sajari/react-hooks';
 import { __DEV__ } from '@sajari/react-sdk-utils';
 import tw from 'twin.macro';
 
@@ -14,7 +14,14 @@ const defaultSizes = [15, 25, 50, 100];
 const PageSize = (props: PageSizeProps) => {
   const { label = 'Show', sizes = defaultSizes, size } = props;
   const { pageSize, setPageSize } = usePageSize();
+  const { searched, totalResults } = useSearchContext();
   const id = `page-size-${useId()}`;
+  const sizesSorted = sizes.sort((a, b) => a - b);
+  const [min] = sizesSorted;
+
+  if ((searched && totalResults === 0) || min > totalResults) {
+    return null;
+  }
 
   return (
     <div css={tw`flex items-center space-x-4`}>
