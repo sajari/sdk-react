f744d41fa7ff25c21476a8ba83cdba28d1f43dae
Tuan Dao <tdao@sajari.com>
2020-11-27 19:01:14 +0700

    refactor: improve performance

--- packages/hooks/src/SearchContextProvider/index.tsx
+++ packages/hooks/src/SearchContextProvider/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/named */
 /* eslint-disable @typescript-eslint/no-shadow */
-import { createContext, isEmpty, useDebounce } from '@sajari/react-sdk-utils';
+import { createContext, isEmpty } from '@sajari/react-sdk-utils';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { Config, defaultConfig } from './config';
@@ -83,11 +83,25 @@ const SearchContextProvider: React.FC<SearchProviderValues> = ({
   const [searchState, setSearchState] = useState(defaultState);
   const [instantState, setInstantState] = useState(defaultState);
   const [viewType, setViewType] = useState<ResultViewType>('list');
+  const [configDone, setConfigDone] = useState(false);
+  const searchTimer = useRef<ReturnType<typeof setTimeout>>();
+  const searchInstantTimer = useRef<ReturnType<typeof setTimeout>>();
   const instant = useRef(instantProp);
   const response = search.pipeline.getResponse();
 
-  if (!search.variables) {
+  if (!search.variables && !configDone) {
     Object.assign(search, { variables: new Variables() });
+  }
+
+  if (search.filters && !configDone) {
+    const filter = combineFilters(search.filters);
+
+    search.variables.set({
+      filter: isEmpty(filter.filter()) ? '_id != ""' : () => filter.filter(),
+      countFilters: () => filter.countFilters(),
+      buckets: () => filter.buckets(),
+      count: () => filter.count(),
+    });
   }
 
   useEffect(() => {
@@ -113,13 +127,6 @@ const SearchContextProvider: React.FC<SearchProviderValues> = ({
         filter.listen(EVENT_SELECTION_UPDATED, () => search.pipeline.search(search.variables.get())),
         filter.removeChildFilterListeners,
       );
-
-      search.variables.set({
-        filter: () => filter.filter(),
-        countFilters: () => filter.countFilters(),
-        buckets: () => filter.buckets(),
-        count: () => filter.count(),
-      });
     }
 
     let searchRenderTimer: ReturnType<typeof setTimeout>;
@@ -188,39 +195,43 @@ const SearchContextProvider: React.FC<SearchProviderValues> = ({
       search.pipeline.search(search.variables.get());
     }
 
+    setConfigDone(true);
     return () => {
       unregisterFunctions.forEach((fn) => fn());
     };
   }, []);
 
   const searchFn = useCallback(
-    (key: 'search' | 'instant') =>
-      useDebounce((inputQuery?: string, override: boolean = false) => {
-        const func = key === 'instant' ? instant.current : search;
-        const state = key === 'instant' ? instantState : searchState;
-        const setSearchingState = key === 'instant' ? setInstantSearching : setSearching;
-        setSearchingState(true);
-        const { pipeline, variables } = func as ProviderPipelineConfig;
-        const { config } = state;
+    (key: 'search' | 'instant') => (inputQuery?: string, override: boolean = false) => {
+      const func = key === 'instant' ? instant.current : search;
+      const state = key === 'instant' ? instantState : searchState;
+      const setSearchingState = key === 'instant' ? setInstantSearching : setSearching;
+      const timer = key === 'instant' ? searchInstantTimer : searchTimer;
+      setSearchingState(true);
+      setSearchState((state) => ({ ...state, query: inputQuery ?? state.query }));
+      const { pipeline, variables } = func as ProviderPipelineConfig;
+      const { config } = state;
 
-        const text = {
-          [config.qParam]: inputQuery ?? searchState.query,
-          [config.qOverrideParam]: undefined,
-          [config.pageParam]: undefined,
-        };
+      const text = {
+        [config.qParam]: inputQuery ?? variables.get()[config.qParam],
+        [config.qOverrideParam]: undefined,
+        [config.pageParam]: undefined,
+      };
 
-        if (override) {
-          text[config.qOverrideParam] = 'true';
-        }
+      // TODO(tuand): ask the backend the use of this property
+      if (override) {
+        text[config.qOverrideParam] = 'true';
+      }
 
-        variables.set(text);
-        const { filter } = variables.get();
-        if (isEmpty(filter)) {
-          variables.set({ filter: '_id != ""' });
-        }
+      variables.set(text);
+
+      // @ts-ignore
+      clearTimeout(timer.current);
+      timer.current = setTimeout(() => {
         pipeline.search(variables.get());
-      }, 50),
-    [searchState.query],
+      }, 50);
+    },
+    [],
   );
 
   const clear = useCallback(
--- packages/hooks/src/useSearch/index.ts
+++ packages/hooks/src/useSearch/index.ts
@@ -5,7 +5,6 @@ import { useContext } from '../SearchContextProvider';
 import { defaultConfig } from '../SearchContextProvider/config';
 import { Response } from '../SearchContextProvider/controllers';
 import { EVENT_RESPONSE_UPDATED, EVENT_VALUES_UPDATED } from '../SearchContextProvider/events';
-import useQuery from '../useQuery';
 import { UseSearchConfig, UseSearchCustomConfig, UseSearchParams, UseSearchResult } from './types';
 
 function useCustomSearch({ pipeline, variables }: UseSearchCustomConfig): UseSearchResult {
@@ -51,34 +50,23 @@ function useCustomSearch({ pipeline, variables }: UseSearchCustomConfig): UseSea
   return { ...searchOutput, searching };
 }
 
-function useNormalSearch({ queryOverride = '', allowEmptySearch = true }: UseSearchConfig): UseSearchResult {
+function useNormalSearch({ queryOverride, allowEmptySearch = true }: UseSearchConfig): UseSearchResult {
   const [error, setError] = useState<Error | null>(null);
-  const { query } = useQuery();
   const {
-    search: { searching, response, search: searchFn },
+    search: { searching, response, search },
     instant: { search: searchInstantFn, suggestions },
   } = useContext();
 
   const results = response?.getResults();
-
-  const search = useCallback(
-    (q?: string) => {
-      if (q !== undefined) {
-        searchFn(q);
-      } else {
-        searchFn(query);
-      }
-    },
-    [query],
-  );
-
   const searchInstant = useCallback((q: string) => searchInstantFn(q), []);
 
   useEffect(() => {
-    if ((queryOverride === '' && allowEmptySearch) || queryOverride !== '') {
-      searchFn(queryOverride);
+    if ((queryOverride === '' && allowEmptySearch) || (queryOverride && queryOverride === '')) {
+      search(queryOverride);
+    } else {
+      search();
     }
-  }, [queryOverride, allowEmptySearch]);
+  }, [queryOverride]);
 
   useEffect(() => {
     if (response) {
@@ -111,7 +99,7 @@ function useSearch(params?: UseSearchParams) {
   if (params !== undefined && typeof params === 'object' && 'pipeline' in params && 'variables' in params) {
     return useCustomSearch(params);
   }
-  return useNormalSearch(params || {});
+  return useNormalSearch(params ?? {});
 }
 
 export default useSearch;
