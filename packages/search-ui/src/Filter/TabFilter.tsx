1cd6da22626ca702445945d9b597d92ed078a67b
Sam Potts <sam@potts.es>
2020-12-01 15:52:46 +1100

    chore: add missing jsx pragma to TabFilter

--- packages/search-ui/src/Filter/TabFilter.tsx
+++ packages/search-ui/src/Filter/TabFilter.tsx
@@ -1,7 +1,8 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import { Tab, TabList, Tabs } from '@sajari/react-components';
 import { useFilter } from '@sajari/react-hooks';
 import { isEmpty } from '@sajari/react-sdk-utils';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { TabFilterProps } from './types';
