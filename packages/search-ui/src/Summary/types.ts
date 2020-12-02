29e0ec7ac0388fd353572fc72d34005d0be885fc
Tuan Dao <tdao@sajari.com>
2020-12-02 21:46:19 +0700

    fix: summary disable suggestion by default

--- packages/search-ui/src/Summary/types.ts
+++ packages/search-ui/src/Summary/types.ts
@@ -2,7 +2,7 @@ import { BoxProps, TextProps } from '@sajari/react-components';
 
 export interface SummaryProps extends BoxProps, Omit<TextProps, 'children'> {
   /** Show the query override text */
-  showOverride?: boolean;
+  suggest?: boolean;
   /** Show the search latency  */
   showLatency?: boolean;
 }
