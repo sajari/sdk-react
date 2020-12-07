1664ca26f13d43914ee37c06e9719c3be4c7019b
Tuan Dao <tdao@sajari.com>
2020-12-07 22:45:23 +0700

    chore: add test for pure pagination

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,6 +1,7 @@
 module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/jest-preprocess.js',
+    '^.+\\.svg$': 'jest-svg-transformer',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
