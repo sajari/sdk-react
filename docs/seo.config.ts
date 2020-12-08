bd5ea7dd61a153b01e4e255fa546a87b04c0b523
Sam Potts <sam@potts.es>
2020-12-08 16:47:24 +1100

    docs: tweak to meta description

--- docs/seo.config.ts
+++ docs/seo.config.ts
@@ -1,10 +1,10 @@
 const SEO = {
   title: 'Sajari React SDK',
-  description: 'Build a beautiful search interface using our powerful React SDK.',
+  description: 'Build a beautiful and accessible search interface using our powerful React SDK.',
   openGraph: {
     url: 'https://sajari.com',
     title: 'Sajari React SDK',
-    description: 'Build a beautiful search interface using our powerful React SDK.',
+    description: 'Build a beautiful and accessible search interface using our powerful React SDK.',
     site_name: 'Sajari React SDK',
   },
   twitter: {
