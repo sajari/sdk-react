3bfbc62be5c90a73351b0cd47a2b359a49d6c1ab
Sam Potts <sam@potts.es>
2020-12-08 15:31:57 +1100

    docs: update meta descriptions

--- docs/seo.config.ts
+++ docs/seo.config.ts
@@ -1,25 +1,11 @@
 const SEO = {
-  title: 'Sajari SDK | Design System built with React',
-  description: 'Simple, Modular and Accessible UI Components for React Applications. Built with Tailwind.',
+  title: 'Sajari React SDK',
+  description: 'Build a beautiful search interface using our powerful React SDK.',
   openGraph: {
     url: 'https://sajari.com',
-    title: 'Sajari SDK',
-    description: 'Simple, Modular and Accessible UI Components for React Applications. Built with Tailwind.',
-    /* images: [
-      {
-        url: 'https://res.cloudinary.com/adebayosegun/image/upload/v1567591645/Sajari%20UI/og-image.png',
-        width: 1012,
-        height: 506,
-        alt: 'Sajari SDK Library - Powered by React and Styled-System',
-      },
-      {
-        url: 'https://res.cloudinary.com/adebayosegun/image/upload/v1567591645/Sajari%20UI/twitter-og-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Sajari SDK Library - Powered by React and Styled-System',
-      },
-    ], */
-    site_name: 'Sajari SDK',
+    title: 'Sajari React SDK',
+    description: 'Build a beautiful search interface using our powerful React SDK.',
+    site_name: 'Sajari React SDK',
   },
   twitter: {
     handle: '@sajari-ui',
