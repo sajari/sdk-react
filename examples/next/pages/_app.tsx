1b3a1e542db1224279df76a4b0383ec11fa2972e
Sam Potts <sam@potts.es>
2020-12-10 12:01:27 +1100

    docs: finish the next example for SSR

---
+++ examples/next/pages/_app.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import './app.css';
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { SSRProvider } from '@sajari/react-components';
+
+const title = 'React SDK';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <SSRProvider>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta title={title} />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <Component {...pageProps} />
+  </SSRProvider>
+);
+
+export default App;
--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -1,14 +1,25 @@
-/* eslint-disable react/require-default-props */
-/* eslint-disable react/no-unused-prop-types */
 import React from 'react';
-import { SearchProvider, FieldDictionary, Pipeline, Variables, Results, Input } from '@sajari/react-search-ui';
-import { getResponse } from '@sajari/server';
+import {
+  SearchProvider,
+  FieldDictionary,
+  Pipeline,
+  Variables,
+  Results,
+  Input,
+  SSRProvider,
+  Pagination,
+} from '@sajari/react-search-ui';
+import { search } from '@sajari/server';
+import { useSearchContext } from '@sajari/react-hooks';
 
 const pipeline = new Pipeline(
   {
     account: '1594153711901724220',
     collection: 'bestbuy',
     endpoint: 'https://jsonapi-us-valkyrie.sajari.net',
+    // Key and secret must be populated with details from the credentials page in the Sajari console
+    key: '',
+    secret: '',
   },
   'query',
 );
@@ -19,7 +30,7 @@ const fields = new FieldDictionary({
 });
 
 const variables = new Variables({
-  resultsPerPage: 6,
+  resultsPerPage: 10,
   q: 'iphone',
 });
 
@@ -29,13 +40,22 @@ interface Props {
 
 // This gets called on every request
 export async function getServerSideProps() {
-  const initialResponse = await getResponse({
+  const initialResponse = await search({
     search: {
       pipeline,
       variables,
       fields,
     },
   });
+
+  // If we couldn't get an initial response server side, render client side
+  if (initialResponse === null) {
+    return {
+      props: {
+        notFound: true,
+      },
+    };
+  }
 
   // Pass data to the page via props
   return {
@@ -45,6 +65,17 @@ export async function getServerSideProps() {
   };
 }
 
+const RawResults = () => {
+  const { results } = useSearchContext();
+
+  return (
+    <details>
+      <summary>Raw Results</summary>
+      <pre>{JSON.stringify(results, null, 2)}</pre>
+    </details>
+  );
+};
+
 const Page = ({ initialResponse }: Props) => (
   <SearchProvider
     search={{
@@ -53,9 +84,14 @@ const Page = ({ initialResponse }: Props) => (
       variables,
     }}
     initialResponse={initialResponse}
+    searchOnLoad={!initialResponse}
   >
-    <Input />
-    <Results appearance="grid" />
+    <SSRProvider>
+      <Input />
+      <Results appearance="grid" style={{ margin: '3rem 0' }} />
+      <RawResults />
+      <Pagination />
+    </SSRProvider>
   </SearchProvider>
 );
 
