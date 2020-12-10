92eccab25519f8e33ea88d5ef81a6e54a66f7713
Sam Potts <sam@potts.es>
2020-12-10 12:00:41 +1100

    chore: rename server method to search

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as getResponse } from './get-response';
+export { default as search } from './search';
--- packages/server/src/search/index.tsx
+++ packages/server/src/search/index.tsx
@@ -1,7 +1,7 @@
-import { combineFilters, EVENT_RESPONSE_UPDATED, SearchProviderValues, Variables } from '@sajari/react-hooks';
+import { combineFilters, EVENT_RESPONSE_UPDATED, SearchProviderValues, Variables, Response } from '@sajari/react-hooks';
 import { isEmpty } from '@sajari/react-sdk-utils';
 
-function getResponse(props: SearchProviderValues): Promise<string> {
+function search(props: SearchProviderValues): Promise<string | null> {
   if (!props.search) {
     throw new Error('findResultsState requires a `search` config');
   }
@@ -20,13 +20,18 @@ function getResponse(props: SearchProviderValues): Promise<string> {
   }
 
   return new Promise((resolve) => {
-    const unlisten = pipeline.listen(EVENT_RESPONSE_UPDATED, (response) => {
+    const unlisten = pipeline.listen(EVENT_RESPONSE_UPDATED, (response: Response) => {
       unlisten();
 
+      if (!response || response.isError()) {
+        resolve(null);
+        return;
+      }
+
       const serialized = JSON.stringify({
-        queryValues: Object.fromEntries(response.getQueryValues()),
-        response: Object.fromEntries(response.getResponse()),
-        values: Object.fromEntries(response.getValues()),
+        queryValues: Object.fromEntries(response.getQueryValues() ?? []),
+        response: Object.fromEntries(response.getResponse() ?? []),
+        values: Object.fromEntries(response.getValues() ?? []),
       });
 
       resolve(serialized);
@@ -36,4 +41,4 @@ function getResponse(props: SearchProviderValues): Promise<string> {
   });
 }
 
-export default getResponse;
+export default search;
