f6a65cbfbd5f8ebda3ace887d213a76a569a13b2
Sam Potts <sam@potts.es>
2020-12-14 18:52:25 +1100

    docs: clean up

---
+++ docs/components/PackageTile.tsx
@@ -0,0 +1,40 @@
+import { Box, Heading, Text } from '@sajari-ui/core';
+
+import { BackgroundProps } from '@sajari-ui/core/dist/types';
+
+const PackageTile = ({
+  title,
+  description,
+  href,
+  gradient,
+}: {
+  title: string;
+  description: string;
+  href: string;
+  gradient: BackgroundProps['gradientColorStops'];
+}) => (
+  <Box
+    as="a"
+    href={href}
+    flex="flex-1"
+    textAlign="text-center"
+    borderRadius="rounded-xl"
+    padding="p-6"
+    backgroundColor="bg-gradient-to-br"
+    gradientColorStops={gradient}
+    textColor="text-white"
+    space="space-y-4"
+    style={{ textShadow: '0 1px 3px rgba(0,0,0,.1),0 1px 2px rgba(0,0,0,.06)' }}
+    boxShadow="shadow-md"
+    transition="transition"
+    transitionDuration="duration-150"
+    transform={['transform', 'focus:scale-105', 'hover:scale-105']}
+  >
+    <Heading as="h1" size="lg" textColor="text-inherit">
+      {title}
+    </Heading>
+    <Text>{description}</Text>
+  </Box>
+);
+
+export default PackageTile;
