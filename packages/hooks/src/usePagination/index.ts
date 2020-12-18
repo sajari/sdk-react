2621f46d6b000567ecc809ad34ed34e1ead02b54
Sam Potts <sam@potts.es>
2020-12-18 15:48:28 +1100

    fix: default usePagination argument to ‘search’

--- packages/hooks/src/usePagination/index.ts
+++ packages/hooks/src/usePagination/index.ts
@@ -3,7 +3,7 @@ import { useCallback, useMemo } from 'react';
 import { useContext } from '../ContextProvider';
 import { UsePaginationResult } from './types';
 
-function usePagination(key: 'search' | 'autocomplete'): UsePaginationResult {
+function usePagination(key: 'search' | 'autocomplete' = 'search'): UsePaginationResult {
   const context = useContext();
   const { paginate } = context;
   const { response, config } = context[key];
