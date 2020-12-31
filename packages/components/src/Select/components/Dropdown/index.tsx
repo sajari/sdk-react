cbcfc8a5c471b714f1c13bc40d21ef73c7f8947c
Sam Potts <sam@potts.es>
2020-12-31 18:06:24 +1100

    fix: backward compatibility with <option>

--- packages/components/src/Select/components/Dropdown/index.tsx
+++ packages/components/src/Select/components/Dropdown/index.tsx
@@ -1,15 +1,20 @@
 import { mergeProps } from '@react-aria/utils';
-import { getStylesObject } from '@sajari/react-sdk-utils';
+import { cleanChildren, getStylesObject } from '@sajari/react-sdk-utils';
 import * as React from 'react';
 
 import Box from '../../../Box';
 import { useSelectContext } from '../../context';
+import Option from '../Option';
 import { useDropdownStyles } from './styles';
 
 const Dropdown = () => {
   const { children, open, getMenuProps, disableDefaultStyles, customClassNames } = useSelectContext();
   const { styles: dropdownStyles, focusRingProps } = useDropdownStyles({ shown: open });
   const styles = getStylesObject(dropdownStyles, disableDefaultStyles);
+  // Ensure backwards compatibility with <option> children
+  const options = cleanChildren(children).map((child) =>
+    child.type === 'option' ? <Option {...child.props} key={child.props.value} /> : child,
+  );
 
   return (
     <Box
@@ -18,7 +23,7 @@ const Dropdown = () => {
       {...mergeProps(focusRingProps, getMenuProps())}
       className={customClassNames.dropdownClassName}
     >
-      {open && children}
+      {open && options}
     </Box>
   );
 };
