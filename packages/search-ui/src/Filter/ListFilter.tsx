1ce201be970042f9d40b834020052b019c40942e
Tuan Dao <tdao@sajari.com>
2021-01-05 00:04:04 +0700

    fix: laggy filter search

--- packages/search-ui/src/Filter/ListFilter.tsx
+++ packages/search-ui/src/Filter/ListFilter.tsx
@@ -3,7 +3,9 @@ import { useFilter, useQuery } from '@sajari/react-hooks';
 import { getStylesObject, isBoolean, isEmpty, noop, useTheme } from '@sajari/react-sdk-utils';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useVirtual } from 'react-virtual';
 import tw from 'twin.macro';
+import useResizeObserver from 'use-resize-observer/polyfilled';
 
 import { IconSmallChevronDown, IconSmallChevronUp } from '../assets/icons';
 import { useSearchUIContext } from '../ContextProvider';
@@ -33,10 +35,13 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
   const { disableDefaultStyles = false, customClassNames, language } = useSearchUIContext();
   const theme = useTheme();
   const { t } = useTranslation('filter');
+  const parentRef = React.useRef<HTMLDivElement | null>(null);
+  const { ref, width } = useResizeObserver();
 
   const styles = getStylesObject(
     {
       innerList: [tw`flex items-center justify-between`],
+      innerVirtualizedList: [tw`flex items-center justify-between absolute top-0 left-0 w-full`],
       count: [tw`ml-2 text-xs text-gray-400`],
       checkbox: [tw`text-sm`],
       searchWrapper: [tw`mb-2`],
@@ -56,11 +61,6 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
     setQuery('');
   }, [q]);
 
-  // Collapse when filtering via internal or external query change
-  React.useEffect(() => {
-    setExpanded(false);
-  }, [query, q]);
-
   const Control = multi ? Checkbox : Radio;
   const filtered = searchable ? options.filter((o) => o.label.toLowerCase().includes(query.toLowerCase())) : options;
   const slice = filtered.length > limit;
@@ -79,34 +79,65 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
     return list;
   }, [JSON.stringify(filtered), JSON.stringify(selected), pinSelected, sort, sortAscending]);
 
-  const items = slice && !expanded ? sortedItems.slice(0, limit) : sortedItems;
+  const rowVirtualizer = useVirtual({
+    parentRef,
+    size: sortedItems.length,
+    // Recalculate item's height on change
+    estimateSize: React.useCallback(() => 21, [JSON.stringify(selected), width]),
+    overscan: 10,
+  });
+
+  const items = sortedItems.slice(0, limit);
   const Icon = expanded ? IconSmallChevronUp : IconSmallChevronDown;
 
-  const innerList = React.useMemo(
-    () =>
-      items.map(({ label, count }) => (
-        <CoreBox css={styles.innerList} key={`${label}-${count}`}>
-          <Control
-            value={label}
-            checked={selected.includes(label)}
-            onChange={noop}
-            css={styles.checkbox}
-            disableDefaultStyles={disableDefaultStyles}
-          >
-            {typeof itemRender === 'function' ? itemRender(label) : formatLabel(label, { format, currency, t })}
-          </Control>
-          <span css={styles.count}>{count.toLocaleString(language)}</span>
-        </CoreBox>
-      )),
-    [JSON.stringify(items), itemRender, selected],
-  );
+  const innerList = React.useMemo(() => {
+    const InnerControl = ({ label, count }: { label: string; count: number }) => (
+      <>
+        <Control
+          value={label}
+          checked={selected.includes(label)}
+          onChange={noop}
+          css={styles.checkbox}
+          disableDefaultStyles={disableDefaultStyles}
+        >
+          {typeof itemRender === 'function' ? itemRender(label) : formatLabel(label, { format, currency, t })}
+        </Control>
+        <span css={styles.count}>{count.toLocaleString(language)}</span>
+      </>
+    );
+
+    // If the list is expanded or over the limit, virtualize it, othewise show normal list.
+    return !expanded || !slice
+      ? items.map(({ label, count }) => (
+          <CoreBox css={styles.innerList} key={`${label}-${count}`}>
+            <InnerControl label={label} count={count} />
+          </CoreBox>
+        ))
+      : rowVirtualizer.virtualItems.map(({ index, measureRef, start }) => {
+          const { label, count } = sortedItems[index];
+          return (
+            <CoreBox
+              style={{ transform: `translateY(${start}px)`, margin: 0 }}
+              css={styles.innerVirtualizedList?.concat(index === 0 ? tw`pt-0` : tw`pt-1`)}
+              key={`${label}-${count}`}
+              ref={measureRef}
+            >
+              <InnerControl label={label} count={count} />
+            </CoreBox>
+          );
+        });
+  }, [expanded, slice, rowVirtualizer, itemRender, selected]);
+
+  const filterSearchOnChange = React.useCallback((value) => {
+    setQuery(value || '');
+  }, []);
 
   if (isEmpty(options)) {
     return null;
   }
 
   return (
-    <Box title={title} name={name} showReset={selected.length > 0 && multi} onReset={reset}>
+    <Box ref={ref} title={title} name={name} showReset={selected.length > 0 && multi} onReset={reset}>
       {searchable ? (
         <CoreBox css={styles.searchWrapper}>
           <Combobox
@@ -114,15 +145,18 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
             size="sm"
             className={customClassNames.filter?.list?.searchFilter}
             placeholder={placeholder}
-            onChange={(value) => {
-              setQuery(value || '');
-            }}
+            onChange={filterSearchOnChange}
             disableDefaultStyles={disableDefaultStyles}
           />
         </CoreBox>
       ) : null}
 
-      <CoreBox id={`list-${name}`} className={customClassNames.filter?.list?.container}>
+      <CoreBox
+        id={`list-${name}`}
+        css={slice && expanded ? tw`overflow-auto max-h-96` : tw``}
+        className={customClassNames.filter?.list?.container}
+        ref={parentRef}
+      >
         {multi ? (
           <CheckboxGroup
             value={selected}
@@ -130,6 +164,8 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
             className={customClassNames.filter?.list?.checkboxGroup}
             disableDefaultStyles={disableDefaultStyles}
             aria-labelledby={getHeaderId(name)}
+            style={{ height: slice && expanded ? `${rowVirtualizer.totalSize}px` : 'auto' }}
+            css={tw`relative`}
           >
             {innerList}
           </CheckboxGroup>
@@ -140,6 +176,8 @@ const ListFilter = (props: Omit<ListFilterProps, 'type'>) => {
             className={customClassNames.filter?.list?.radioGroup}
             disableDefaultStyles={disableDefaultStyles}
             aria-labelledby={getHeaderId(name)}
+            style={{ height: slice && expanded ? `${rowVirtualizer.totalSize}px` : 'auto' }}
+            css={tw`relative`}
           >
             {innerList}
           </RadioGroup>
