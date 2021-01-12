3b4988a920342b7fba725c1a7940ee45729933f2
Sam Potts <sam@potts.es>
2021-01-12 15:57:44 +1100

    fix: fix message padding on smaller displays

---
+++ .changeset/many-lizards-march.md
@@ -0,0 +1,5 @@
+---
+'@sajari/react-search-ui': patch
+---
+
+fix message padding on smaller displays
--- packages/search-ui/src/Results/components/Message/styles.ts
+++ packages/search-ui/src/Results/components/Message/styles.ts
@@ -3,7 +3,7 @@ import tw from 'twin.macro';
 
 export default function useMessageStyles() {
   const styles = {
-    container: [tw`w-full p-40 text-center`],
+    container: [tw`w-full px-6 py-20 text-center md:py-40 md:px-12`],
     loadingWrapper: [tw`text-gray-500`],
     loadingSpinner: [tw`inline-block w-6 h-6`],
     loadingText: [tw`mt-3`],
