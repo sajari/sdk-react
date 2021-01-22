4b63ad79019baa8316daaeed06a37c99e46e88b0
zlatan <zlatan.pham@gmail.com>
2021-01-22 10:59:58 +0700

    fix: make close button square

--- packages/components/src/Modal/ModalCloseButton/index.tsx
+++ packages/components/src/Modal/ModalCloseButton/index.tsx
@@ -2,27 +2,26 @@ import React from 'react';
 
 import { useModalContext } from '../context';
 import { ModalCloseButtonProps } from './types';
-import Button from '../../Button';
 import { callAllHandlers, __DEV__, getStylesObject } from '@sajari/react-sdk-utils';
 import { IconClose } from '../../assets/icons';
 import useModalCloseButtonStyles from './styles';
 
-const ModalCloseButton = React.forwardRef((props: ModalCloseButtonProps, ref?: React.Ref<HTMLElement>) => {
+const ModalCloseButton = React.forwardRef((props: ModalCloseButtonProps, ref?: React.Ref<HTMLButtonElement>) => {
   const { onClose, disableDefaultStyles = false } = useModalContext();
   const { onClick, label = 'Close', styles: stylesProp, ...rest } = props;
   const styles = getStylesObject(useModalCloseButtonStyles(), disableDefaultStyles);
 
   return (
-    <Button
+    <button
       ref={ref}
       onClick={callAllHandlers(onClick, onClose)}
       aria-label={label}
-      disableDefaultStyles={disableDefaultStyles}
       css={[styles.container, stylesProp]}
       {...rest}
     >
-      <IconClose />
-    </Button>
+      &#8203;
+      <IconClose css={styles.icon}/>
+    </button>
   );
 });
 
--- packages/components/src/Modal/ModalCloseButton/styles.ts
+++ packages/components/src/Modal/ModalCloseButton/styles.ts
@@ -3,7 +3,10 @@ import tw from 'twin.macro';
 
 export default function useModalCloseButtonStyles() {
   const styles = {
-    container: [tw`ml-auto -mr-4`],
+    container: [
+      tw`inline-flex relative items-center justify-center ml-auto -mr-4 px-3 py-2 text-base font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 no-underline bg-transparent hover:bg-gray-100 focus:bg-gray-100 rounded-md border border-transparent transition duration-150 ease-in-out cursor-pointer focus:outline-none select-none`,
+    ],
+    icon: [tw`inline-block align-middle transition-all duration-200 ease-in-out transform fill-current`],
   };
 
   return mapStyles(styles);
