e66270d087721cc2fe2fc9496b411d6b8ca67cf3
Tuan Dao <tdao@sajari.com>
2021-02-03 22:16:22 +0700

    fix: duplicate _id filter

--- packages/hooks/src/ContextProvider/controllers/Variables.ts
+++ packages/hooks/src/ContextProvider/controllers/Variables.ts
@@ -25,7 +25,6 @@ export class Variables {
       Object.entries({
         [defaultConfig.qParam]: '',
         [defaultConfig.resultsPerPageParam]: 15,
-        filter: '_id != ""',
         ...variables,
       }),
     );
