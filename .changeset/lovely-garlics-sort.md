e482cb2403f7c1486447dcfce6a5d8ccc680613d
Sam Potts <sam@potts.es>
2021-02-04 11:29:19 +1100

    docs: fix title issues (#390)
    * docs: fix title issues
    
    * chore: changeset
    
    * chore: logic clean up

---
+++ .changeset/lovely-garlics-sort.md
@@ -0,0 +1,5 @@
+---
+'sajari-sdk-docs': patch
+---
+
+Fixed title issues
--- docs/components/SEO.tsx
+++ docs/components/SEO.tsx
@@ -3,10 +3,16 @@ import * as React from 'react';
 
 import defaultSEO from '../seo.config';
 
-function SEO(props: NextSeoProps) {
-  const seo = { ...defaultSEO, ...props };
+interface SeoProps extends NextSeoProps {
+  scope?: string;
+}
 
-  return <NextSeo {...seo} titleTemplate="Sajari React SDK | %s" />;
+function SEO(props: SeoProps) {
+  const { scope, title: titleProp, ...rest } = props;
+  const title = [titleProp, scope].filter(Boolean).join(' | ');
+  const seo = { ...defaultSEO, ...rest, title };
+
+  return <NextSeo {...seo} titleTemplate="%s | Sajari React SDK" />;
 }
 
 export default SEO;
