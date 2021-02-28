dd75955c31cc21fedae855a6871aec4d2e89c885
jschumacher <jens.schumacher@gmail.com>
2021-03-01 10:46:08 +1100

    Update seo.config.ts

--- docs/seo.config.ts
+++ docs/seo.config.ts
@@ -1,6 +1,6 @@
 const SEO = {
   title: 'Sajari React SDK',
-  description: 'Build a beautiful and accessible search interface using our powerful React SDK.',
+  description: 'Build beautiful and accessible search interfaces with the Sajari React SDK.',
 };
 
 export default SEO;
