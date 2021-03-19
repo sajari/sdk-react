e63197538590003341e74b81e492f1b8f3555d96
zlatan <zlatan.pham@gmail.com>
2021-03-19 10:47:30 +0700

    chore: update docs

--- packages/search-ui/src/Input/types.ts
+++ packages/search-ui/src/Input/types.ts
@@ -3,5 +3,6 @@ import { ComboboxProps } from '@sajari/react-components';
 export interface InputProps<T>
   extends Pick<ComboboxProps<T>, 'placeholder' | 'onSelect' | 'onChange' | 'inputElement'> {
   mode?: ComboboxProps<T>['mode'] | 'instant';
+  /* Sets how many autocomplete suggestions are shown in the box below the search input */
   maxSuggestions?: number;
 }
