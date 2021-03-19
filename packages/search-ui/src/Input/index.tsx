591766ef0826c4fb347358d78c019841a1cc1696
zlatan <zlatan.pham@gmail.com>
2021-03-19 09:54:25 +0700

    fix: apply maxSuggestions for suggestions mode

--- packages/search-ui/src/Input/index.tsx
+++ packages/search-ui/src/Input/index.tsx
@@ -13,7 +13,14 @@ import { InputProps } from './types';
 
 const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInputElement>) => {
   const { t } = useTranslation('input');
-  const { placeholder = t('placeholder'), mode = 'instant', onSelect, onChange, maxSuggestions = 5, ...rest } = props;
+  const {
+    placeholder = t('placeholder'),
+    mode = 'instant',
+    onSelect,
+    onChange,
+    maxSuggestions = mode === 'results' ? 5 : 10,
+    ...rest
+  } = props;
   const { results: rawResults, search, searching, fields } = useSearchContext();
   const results = React.useMemo(() => mapResultFields<ResultValues>(rawResults ?? [], fields), [rawResults]);
   const { search: searchAutocomplete, completion, suggestions } = useAutocomplete();
@@ -22,7 +29,7 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
   let items: Array<any> = [];
 
   if (mode === 'suggestions') {
-    items = suggestions;
+    items = suggestions.splice(0, maxSuggestions);
   } else if (mode === 'results') {
     items = results.splice(0, maxSuggestions).map((result) => {
       const { values, token } = result;
