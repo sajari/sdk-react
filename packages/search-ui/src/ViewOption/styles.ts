848b332d777b1f497cb0f88375212ec234602007
Thanh Pham <zlatan.pham@gmail.com>
2021-05-11 09:49:07 +0700

    feat: add list display for sorting component (#486)

--- packages/search-ui/src/ViewOption/styles.ts
+++ packages/search-ui/src/ViewOption/styles.ts
@@ -9,7 +9,10 @@ export default function useViewOptionStyles(params: UseViewOptionStyleParams) {
   const { inline } = params;
   const styles = {
     container: [tw`min-w-0`, inline ? tw`flex items-center space-x-2` : tw`space-y-1`],
-    label: [tw`text-gray-500`],
+    label: [
+      tw`text-gray-500`,
+      inline ? undefined : tw`text-gray-400 font-medium tracking-wide leading-snug text-xs uppercase`,
+    ].filter(Boolean),
   };
 
   return mapStyles(styles);
