92a874404802b5b219f6e03e45116e90640df4ef
TuanD <heryhudson@gmail.com>
2021-06-17 19:45:36 +0700

    feat: add reset button when error (#521)
    Co-authored-by: Tuan Dao <tuand@dwarvesv.com>

--- packages/search-ui/src/Results/components/Message/types.ts
+++ packages/search-ui/src/Results/components/Message/types.ts
@@ -3,6 +3,7 @@ import { BoxProps } from '@sajari/react-components';
 export interface MessageProps extends BoxProps {
   title: string;
   body?: string;
+  showReset?: boolean;
   loading?: boolean;
   appearance?: 'default' | 'loading' | 'error';
 }
