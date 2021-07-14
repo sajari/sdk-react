0232fb41444d3e0365b2ffa81d75e49bc2849cb6
Tuan Dao <tuand@dwarvesv.com>
2021-07-14 11:41:46 +0700

    feat: add internationalization

--- packages/search-ui/src/i18n/index.ts
+++ packages/search-ui/src/i18n/index.ts
@@ -2,10 +2,16 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './locale/en';
+import vi from './locale/vi';
 
 i18n.use(LanguageDetector).init({
+  detection: {
+    // we would want to look at html lang attr first
+    order: ['htmlTag', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator'],
+  },
   resources: {
     en,
+    vi,
   },
   fallbackLng: 'en',
   interpolation: {
