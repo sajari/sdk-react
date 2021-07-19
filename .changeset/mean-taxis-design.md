366b3de997b7854061b7d1be101298109ad7634e
TuanD <heryhudson@gmail.com>
2021-07-19 10:43:55 +0700

    fix: minimum character flicker (#559)
    * fix: minium character flicker
    
    * resolve comments
    
    * resolve comments
    
    Co-authored-by: Tuan Dao <tuand@dwarvesv.com>

---
+++ .changeset/mean-taxis-design.md
@@ -0,0 +1,7 @@
+---
+'sajari-sdk-docs': patch
+'@sajari/react-components': patch
+'@sajari/react-search-ui': patch
+---
+
+Fix the dropdown flicker issue when using minimum characters
--- packages/search-ui/src/Input/index.tsx
+++ packages/search-ui/src/Input/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Combobox } from '@sajari/react-components';
 import { useAutocomplete, useQuery, useSearchContext } from '@sajari/react-hooks';
-import { __DEV__, isArray } from '@sajari/react-sdk-utils';
+import { __DEV__, arraysEqual, isArray } from '@sajari/react-sdk-utils';
 import classnames from 'classnames';
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useSearchUIContext } from '../ContextProvider';
@@ -31,58 +31,75 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
   const { search: searchAutocompleteFunc, completion, suggestions } = useAutocomplete();
   const { customClassNames, disableDefaultStyles = false, tracking } = useSearchUIContext();
   const { query } = useQuery();
-  // Use in the minimumCharacters implementation
-  const lastValue = useRef('');
-  let items: Array<any> = [];
+  const [internalSuggestions, setInternalSuggestions] = useState<Array<any>>([]);
+  const [items, setItems] = useState(internalSuggestions);
+  const lastItems = useRef(items);
 
   const search = useCallback(
     (value: string) => {
       if (value.length >= minimumCharacters) {
+        if (!retainFilters) {
+          resetFilters();
+        }
+
         searchFunc(value);
       }
     },
-    [searchFunc, minimumCharacters],
+    [searchFunc, minimumCharacters, retainFilters, resetFilters],
   );
 
   const searchAutocomplete = useCallback(
     (value: string) => {
       if (value.length >= minimumCharacters) {
+        if (!retainFilters) {
+          resetFilters();
+        }
+
         searchAutocompleteFunc(value);
-      } else if (lastValue.current.length >= minimumCharacters) {
-        // Intentionally search for an empty string to clear the old dropdown list when the number of characters is below the limit
-        searchAutocompleteFunc('');
       }
-      lastValue.current = value;
     },
-    [searchAutocompleteFunc, minimumCharacters],
+    [searchAutocompleteFunc, minimumCharacters, retainFilters, resetFilters],
   );
 
-  if (mode === 'suggestions') {
-    items = suggestions.splice(0, maxSuggestions);
-  } else if (mode === 'results') {
-    items = results.splice(0, maxSuggestions).map((result) => {
-      const { values, token } = result;
-      const { description, image, title } = values;
-      const { href, onClick } = useClickTracking({ tracking, values, token });
+  useEffect(() => {
+    if (mode === 'suggestions') {
+      setInternalSuggestions(suggestions.splice(0, maxSuggestions));
+    } else if (mode === 'results') {
+      setInternalSuggestions(
+        results.splice(0, maxSuggestions).map((result) => {
+          const { values, token } = result;
+          const { description, image, title } = values;
+          const { href, onClick } = useClickTracking({ tracking, values, token });
 
-      return {
-        title,
-        url: href,
-        onClick,
-        description,
-        image: isArray(image) ? image[0] : image,
-      };
-    });
-  }
+          return {
+            title,
+            url: href,
+            onClick,
+            description,
+            image: isArray(image) ? image[0] : image,
+          };
+        }),
+      );
+    }
+  }, [mode, suggestions, maxSuggestions, results, useClickTracking, tracking]);
+
+  useEffect(() => {
+    if (!arraysEqual(lastItems.current, internalSuggestions)) {
+      setItems(internalSuggestions);
+      lastItems.current = internalSuggestions;
+    } else {
+      lastItems.current = items;
+    }
+  }, [internalSuggestions, minimumCharacters]);
 
   const onChangeMemoized = useCallback(
     (value) => {
-      if (!retainFilters) {
-        resetFilters();
-      }
-
       if (onChange) {
         onChange(value);
+      }
+
+      if (value.length < minimumCharacters) {
+        setItems([]);
       }
 
       if (mode === 'suggestions' || mode === 'typeahead') {
@@ -93,17 +110,19 @@ const Input = React.forwardRef((props: InputProps<any>, ref: React.Ref<HTMLInput
         search(value);
       }
     },
-    [onChange, mode, search],
+    [onChange, mode, search, minimumCharacters],
   );
 
   const onVoiceInput = useCallback(
     (value) => {
-      if (!retainFilters) {
-        resetFilters();
-      }
       if (onChange) {
         onChange(value);
       }
+
+      if (value.length < minimumCharacters) {
+        setItems([]);
+      }
+
       search(value);
     },
     [search, onChange],
