09b60a4533ffb68102d69dc2b75139091828e831
Tuan Dao <tuand@dwarvesv.com>
2021-07-21 18:32:10 +0700

    fix: clicking in range input does not update with right value

---
+++ .changeset/small-ears-mix.md
@@ -0,0 +1,6 @@
+---
+'@sajari/react-components': patch
+'@sajari/react-sdk-utils': patch
+---
+
+Fix a bug where clicking in the RangeInput does not update with the right value
--- packages/utils/src/test/number.test.ts
+++ packages/utils/src/test/number.test.ts
@@ -1,4 +1,4 @@
-import { clamp } from '../number';
+import { clamp, roundToStep } from '../number';
 
 test.each([
   [0, 100, 101, 100],
@@ -9,4 +9,13 @@ test.each([
   [50, 20, 21, 20],
 ])('min: %d, max: %d, input: %d', (min, max, input, result) => {
   expect(clamp(input, min, max)).toBe(result);
+});
+
+test.each([
+  [0, 100, 0.5, 1, 50],
+  [0, 100, 0.05, 1, 5],
+  [300, 700, 0.3, 1, 420],
+  [400, 450, 0.9, 1, 445],
+])('min: %d, max: %d, percent: %d, step: %d', (min, max, percent, step, output) => {
+  expect(roundToStep((max - min) * percent + min, step)).toBe(output);
 });
