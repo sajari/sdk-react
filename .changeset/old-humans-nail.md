2a8312533589843cb643a8e28ba7ce62c8b9e107
Wesley Walser <wwalser@sajari.com>
2021-12-12 23:42:49 +1100

    feat: new favicon

---
+++ .changeset/old-humans-nail.md
@@ -0,0 +1,5 @@
+---
+'sajari-sdk-docs': patch
+---
+
+feat: new favicon
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -82,7 +82,9 @@ const App = ({ Component, pageProps }: AppProps) => (
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#319795" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         </Head>
         <Layout>
