7d08c31c163e581465153352a6f6bd8d8f87df9c
Nick <chidojiro@gmail.com>
2022-03-16 12:17:16 +0700

    fix: make sure pagination is memoized

--- packages/components/src/Pagination/index.tsx
+++ packages/components/src/Pagination/index.tsx
@@ -20,209 +20,212 @@ const defaultI18n = {
   current: 'Page {{page}}, current page',
 };
 
-const Pagination = React.memo((props: PaginationProps) => {
-  const { ref, width } = useResizeObserver<HTMLDivElement>();
-  const {
-    language,
-    totalResults,
-    resultsPerPage,
-    page,
-    pageCount,
-    onChange,
-    i18n: i18nProp,
-    buttonClassName,
-    activeClassName = '',
-    nextClassName,
-    prevClassName,
-    spacerEllipsisClassName,
-    statusClassName,
-    styles: stylesProp,
-    disableDefaultStyles = false,
-    scrollTarget,
-    scrollToTop = false,
-    compact = width && width < 480,
-    ...rest
-  } = props;
-  const firstRender = useFirstRender();
-  const styles = getStylesObject(usePaginationStyles(props), disableDefaultStyles);
-  const i18n = { ...defaultI18n, ...i18nProp };
+const Pagination = React.memo(
+  (props: PaginationProps) => {
+    const { ref, width } = useResizeObserver<HTMLDivElement>();
+    const {
+      language,
+      totalResults,
+      resultsPerPage,
+      page,
+      pageCount,
+      onChange,
+      i18n: i18nProp,
+      buttonClassName,
+      activeClassName = '',
+      nextClassName,
+      prevClassName,
+      spacerEllipsisClassName,
+      statusClassName,
+      styles: stylesProp,
+      disableDefaultStyles = false,
+      scrollTarget,
+      scrollToTop = false,
+      compact = width && width < 480,
+      ...rest
+    } = props;
+    const firstRender = useFirstRender();
+    const styles = getStylesObject(usePaginationStyles(props), disableDefaultStyles);
+    const i18n = { ...defaultI18n, ...i18nProp };
 
-  // Polyfill
-  React.useEffect(() => {
-    if (!isSSR()) {
-      smoothscroll.polyfill();
-    }
-  }, []);
+    // Polyfill
+    React.useEffect(() => {
+      if (!isSSR()) {
+        smoothscroll.polyfill();
+      }
+    }, []);
 
-  // Handle scroll on paginate
-  React.useEffect(() => {
-    if (firstRender || isSSR()) {
-      return;
-    }
-
-    const scrollOptions: ScrollToOptions = { behavior: 'smooth' };
-    let target: Element | null = null;
-
-    if (isString(scrollTarget)) {
-      target = document.querySelector(scrollTarget);
-    } else if (scrollTarget) {
-      target = scrollTarget;
-    }
-
-    // Scroll an element (or document.body) into view
-    if (scrollToTop) {
-      (target ?? document.body).scrollIntoView(scrollOptions);
-    }
-
-    // Scroll within the element if specified
-    if (target) {
-      target.scrollTo({ ...scrollOptions, top: 0 });
-    }
-  }, [page]);
-
-  // Calculate the page count
-  const count = React.useMemo(() => {
-    if (!totalResults || !resultsPerPage) {
-      return undefined;
-    }
-
-    if (!pageCount) {
-      return Math.ceil(totalResults / resultsPerPage);
-    }
-
-    return pageCount;
-  }, [pageCount, totalResults, resultsPerPage]);
-
-  const changeHandler = React.useCallback(
-    (exitEarly: boolean, target: number) => () => {
-      if (target === page || exitEarly) {
+    // Handle scroll on paginate
+    React.useEffect(() => {
+      if (firstRender || isSSR()) {
         return;
       }
 
-      onChange(clamp(target, 1, count));
-    },
-    [onChange, page, count],
-  );
+      const scrollOptions: ScrollToOptions = { behavior: 'smooth' };
+      let target: Element | null = null;
 
-  const renderButtons = () => {
-    const limit = 5;
-    const middle = Math.ceil(limit / 2);
-    let offset = 0;
+      if (isString(scrollTarget)) {
+        target = document.querySelector(scrollTarget);
+      } else if (scrollTarget) {
+        target = scrollTarget;
+      }
 
-    if (!count) {
+      // Scroll an element (or document.body) into view
+      if (scrollToTop) {
+        (target ?? document.body).scrollIntoView(scrollOptions);
+      }
+
+      // Scroll within the element if specified
+      if (target) {
+        target.scrollTo({ ...scrollOptions, top: 0 });
+      }
+    }, [page]);
+
+    // Calculate the page count
+    const count = React.useMemo(() => {
+      if (!totalResults || !resultsPerPage) {
+        return undefined;
+      }
+
+      if (!pageCount) {
+        return Math.ceil(totalResults / resultsPerPage);
+      }
+
+      return pageCount;
+    }, [pageCount, totalResults, resultsPerPage]);
+
+    const changeHandler = React.useCallback(
+      (exitEarly: boolean, target: number) => () => {
+        if (target === page || exitEarly) {
+          return;
+        }
+
+        onChange(clamp(target, 1, count));
+      },
+      [onChange, page, count],
+    );
+
+    const renderButtons = () => {
+      const limit = 5;
+      const middle = Math.ceil(limit / 2);
+      let offset = 0;
+
+      if (!count) {
+        return null;
+      }
+
+      if (count > limit) {
+        if (page < limit) {
+          offset = 0;
+        } else {
+          const max = count - limit;
+          offset = clamp(page > max ? max : page - middle, 0, max);
+        }
+      }
+
+      const items: Array<number | null> = Array.from(Array(clamp(count, 0, limit))).map(
+        (_, index) => Number(index) + offset,
+      );
+
+      if (count > limit) {
+        // Add the 1 ...
+        if (offset > 1) {
+          items.unshift(0, null);
+        }
+
+        // Add the ... last
+        const lastIndex = count - 1;
+
+        if (!items.includes(lastIndex) && count > limit + 2) {
+          items.push(null, lastIndex);
+        }
+      }
+
+      return items.map((item, index) => {
+        if (item === null) {
+          return (
+            <Box
+              as="span"
+              key={`spacer-ellipsis-${index}`} // eslint-disable-line
+              css={styles.spacerEllipsis}
+              className={spacerEllipsisClassName}
+            >
+              &hellip;
+            </Box>
+          );
+        }
+
+        const number = item + 1;
+        const active = number === page;
+        const template = active ? i18n.current : i18n.page;
+        const ariaLabel = isFunction(template) ? template({ page: number }) : template;
+
+        return (
+          <Button
+            key={item}
+            appearance={active ? 'primary' : undefined}
+            aria-current={active ? 'page' : undefined}
+            aria-label={ariaLabel}
+            onClick={changeHandler(false, number)}
+            className={classnames(buttonClassName, { [activeClassName]: active })}
+            css={tw`m-0`}
+          >
+            {number.toLocaleString(language)}
+          </Button>
+        );
+      });
+    };
+
+    if (!count || count <= 1) {
       return null;
     }
 
-    if (count > limit) {
-      if (page < limit) {
-        offset = 0;
-      } else {
-        const max = count - limit;
-        offset = clamp(page > max ? max : page - middle, 0, max);
-      }
-    }
+    const hasPrevious = page > 1;
+    const hasNext = page < count;
 
-    const items: Array<number | null> = Array.from(Array(clamp(count, 0, limit))).map(
-      (_, index) => Number(index) + offset,
-    );
-
-    if (count > limit) {
-      // Add the 1 ...
-      if (offset > 1) {
-        items.unshift(0, null);
-      }
-
-      // Add the ... last
-      const lastIndex = count - 1;
-
-      if (!items.includes(lastIndex) && count > limit + 2) {
-        items.push(null, lastIndex);
-      }
-    }
-
-    return items.map((item, index) => {
-      if (item === null) {
-        return (
-          <Box
-            as="span"
-            key={`spacer-ellipsis-${index}`} // eslint-disable-line
-            css={styles.spacerEllipsis}
-            className={spacerEllipsisClassName}
-          >
-            &hellip;
-          </Box>
-        );
-      }
-
-      const number = item + 1;
-      const active = number === page;
-      const template = active ? i18n.current : i18n.page;
-      const ariaLabel = isFunction(template) ? template({ page: number }) : template;
-
-      return (
+    return (
+      <ButtonGroup as="nav" ref={ref} aria-label={i18n.label} attached css={[styles.container, stylesProp]} {...rest}>
         <Button
-          key={item}
-          appearance={active ? 'primary' : undefined}
-          aria-current={active ? 'page' : undefined}
-          aria-label={ariaLabel}
-          onClick={changeHandler(false, number)}
-          className={classnames(buttonClassName, { [activeClassName]: active })}
+          spacing="compact"
+          disabled={!hasPrevious}
+          onClick={changeHandler(!hasPrevious, page - 1)}
+          aria-label={i18n.previous}
+          className={classnames(prevClassName, buttonClassName)}
+          rel="prev"
+          key="prev"
           css={tw`m-0`}
         >
-          {number.toLocaleString(language)}
+          &#8203;
+          <IconChevronLeft />
         </Button>
-      );
-    });
-  };
 
-  if (!count || count <= 1) {
-    return null;
-  }
+        {compact ? (
+          <Box as="span" css={styles.compactStatus} className={statusClassName} key="status">
+            {page.toLocaleString(language)}
+            <Box as="span" css={styles.compactStatusCount}>{` / ${count.toLocaleString(language)}`}</Box>
+          </Box>
+        ) : (
+          renderButtons()
+        )}
 
-  const hasPrevious = page > 1;
-  const hasNext = page < count;
-
-  return (
-    <ButtonGroup as="nav" ref={ref} aria-label={i18n.label} attached css={[styles.container, stylesProp]} {...rest}>
-      <Button
-        spacing="compact"
-        disabled={!hasPrevious}
-        onClick={changeHandler(!hasPrevious, page - 1)}
-        aria-label={i18n.previous}
-        className={classnames(prevClassName, buttonClassName)}
-        rel="prev"
-        key="prev"
-        css={tw`m-0`}
-      >
-        &#8203;
-        <IconChevronLeft />
-      </Button>
-
-      {compact ? (
-        <Box as="span" css={styles.compactStatus} className={statusClassName} key="status">
-          {page.toLocaleString(language)}
-          <Box as="span" css={styles.compactStatusCount}>{` / ${count.toLocaleString(language)}`}</Box>
-        </Box>
-      ) : (
-        renderButtons()
-      )}
-
-      <Button
-        spacing="compact"
-        disabled={!hasNext}
-        onClick={changeHandler(!hasNext, page + 1)}
-        aria-label={i18n.next}
-        className={classnames(nextClassName, buttonClassName)}
-        rel="next"
-        key="next"
-        css={tw`m-0`}
-      >
-        &#8203;
-        <IconChevronRight />
-      </Button>
-    </ButtonGroup>
-  );
-});
+        <Button
+          spacing="compact"
+          disabled={!hasNext}
+          onClick={changeHandler(!hasNext, page + 1)}
+          aria-label={i18n.next}
+          className={classnames(nextClassName, buttonClassName)}
+          rel="next"
+          key="next"
+          css={tw`m-0`}
+        >
+          &#8203;
+          <IconChevronRight />
+        </Button>
+      </ButtonGroup>
+    );
+  },
+  (prev, next) => JSON.stringify(prev) === JSON.stringify(next),
+);
 
 export default Pagination;
 export type { PaginationProps };
